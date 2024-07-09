7955a30d7feb6ec2daf839f7ab09ef9e232357bb
Hadrien <ketsapiwiq@protonmail.com>
2024-07-09 16:14:06 +0200

    feat: reveal screen with models

--- fastchat/serve/languia/utils.py
+++ fastchat/serve/languia/utils.py
@@ -135,19 +135,42 @@ def add_chosen_badge(side, which_model_radio):
         side == "b" and which_model_radio == "rightvote"
     ):
         return """
-         <span class="fr-badge">Votre choix</span>
+         <span class="fr-badge fr-badge--success">Votre choix</span>
          """
     else:
         return ""
 
+def get_model_card(model_name):
+  model = dict()
+  model.is_open = True
+  model.size = "Gabarit moyen"
+  model.license = "Licence MIT"
+  model.link = "https://example.org"
+  return model
+
+# TODO: refacto to custom component?
+def build_model_card(model_name):
+  model = get_model_card(model_name)
+  model_openness = ("Modèle ouvert" if model.is_open else "Modèle fermé") 
+  template = f'''
+  <p><span class="fr-icon-stack" aria-hidden="true"></span> {model_openness}</p>
+  <p><span class="fr-icon-ruler" aria-hidden="true"></span> {model.size}</p>
+  <p><span class="fr-icon-copyright-fill" aria-hidden="true"></span> {model.license}</p>
+  <p><a class="fr-btn" href="{model.link}">En savoir plus</a></p>" 
+  '''
+  return template
 
 def build_reveal_html(model_a, model_b, which_model_radio):
-    reveal_html = f"""<div><div class="fr-tile"><h2>Modèle A : {model_a}</h2>"""
+    reveal_html = f"""<div><h2>Merci pour votre vote !<br />
+Découvrez les modèles avec lesquels vous venez de discuter :</h2>
+<div class="fr-tile"><h2>{model_a}</h2>"""
     reveal_html += add_chosen_badge("a", which_model_radio)
+    reveal_html += build_model_card(model_a)
     reveal_html += f"""</div>
-    <div class="fr-tile"><h2>Modèle B : {model_b}</h2>
+    <div class="fr-tile"><h2>{model_b}</h2>
     """
     reveal_html += add_chosen_badge("b", which_model_radio)
+    reveal_html += build_model_card(model_b)
     reveal_html += "</div></div>"
 
     print(reveal_html)
