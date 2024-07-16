e7eceedbb1d9fc003dd5c4b3594707ed51d455da
Hadrien <ketsapiwiq@protonmail.com>
2024-07-16 17:08:21 +0200

    fix: fix matomo script

--- languia/utils.py
+++ languia/utils.py
@@ -162,25 +162,29 @@ def get_battle_pair(
 
 
 def get_matomo_js(matomo_url, matomo_id):
-    return f"""
+    js = """
     <!-- Matomo -->
 <script>
-  var _paq = wind               ow._paq = window._paq || [];
+  var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
-  _paq.push(['HeatmapSessionRecording::enable'])
-  (function() {{
+  _paq.push(['HeatmapSessionRecording::enable']);
+  (function() {"""
+    js += f"""
     var u="{matomo_url}/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '{os.getenv("MATOMO_ID")}']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  }})();
-</script>
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);"""
+    js += """           
+  })();
+</script>"""
+    js += f"""
 <noscript><p><img referrerpolicy="no-referrer-when-downgrade" src="{matomo_url}/matomo.php?idsite={matomo_id}&amp;rec=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Matomo Code -->
     """
+    return js
 
 
 def add_chosen_badge(side, which_model_radio):
