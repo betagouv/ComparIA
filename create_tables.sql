927395e551b24087d52572d0e68220faef65bb4f
Hadrien <ketsapiwiq@protonmail.com>
2024-08-29 13:51:04 +0200

    fix: schema sql

--- create_tables.sql
+++ create_tables.sql
@@ -11,34 +11,34 @@ CREATE TABLE logs (
 
  CREATE TABLE votes (
             tstamp TIMESTAMP NOT NULL,
-            model_a_name VARCHAR NOT NULL,
-            model_b_name VARCHAR NOT NULL,
+            model_a_name VARCHAR(255) NOT NULL,
+            model_b_name VARCHAR(255) NOT NULL,
             model_pair_name JSONB NOT NULL,
-            chosen_model_name VARCHAR NOT NULL,
-            intensity VARCHAR NOT NULL,
-            opening_prompt VARCHAR NOT NULL,
+            chosen_model_name VARCHAR(255) NOT NULL,
+            intensity VARCHAR(255),
+            opening_prompt VARCHAR(255) NOT NULL,
             conversation_a JSONB NOT NULL,
             conversation_b JSONB NOT NULL,
-            turns INT NOT NULL,
-            selected_category VARCHAR NOT NULL,
-            is_unedited_prompt BOOLEAN NOT NULL,
-            template JSONB NOT NULL,
+            turns INT,
+            selected_category VARCHAR(255),
+            is_unedited_prompt BOOLEAN,
+            template JSONB,
             uuid UUID NOT NULL,
-            ip VARCHAR NOT NULL,
-            session_hash VARCHAR NOT NULL,
-            visitor_uuid UUID NOT NULL,
-            relevance INT NOT NULL,
-            form INT NOT NULL,
-            style INT NOT NULL,
-            comments TEXT NOT NULL
+            ip VARCHAR,
+            session_hash VARCHAR,
+            visitor_uuid VARCHAR,
+            relevance INT,
+            form INT,
+            style INT,
+            comments TEXT
         );
 
 CREATE TABLE profiles (
     tstamp TIMESTAMP NOT NULL,
-    chatbot_use VARCHAR NOT NULL,
-    gender VARCHAR NOT NULL,
-    age VARCHAR NOT NULL,
-    profession VARCHAR NOT NULL,
-    session_hash VARCHAR NOT NULL,
-    extra JSONB NOT NULL
+    chatbot_use VARCHAR(255),
+    gender VARCHAR(255),
+    age VARCHAR(255),
+    profession VARCHAR(255),
+    session_hash VARCHAR(255),
+    extra JSONB
 );
