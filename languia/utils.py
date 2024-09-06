48123f945a2b30327d5d82a71ccebdc03c0cff43
Hadrien <ketsapiwiq@protonmail.com>
2024-09-06 17:19:17 +0200

    feat: both equal

--- languia/utils.py
+++ languia/utils.py
@@ -166,7 +166,6 @@ def get_chosen_model(which_model_radio):
         chosen_model = which_model_radio
     else:
         chosen_model = None
-        raise (ValueError)
     return chosen_model
 
 
