836ca5612ce2d1b166a34396c6e00e4a55dc354f
Hadrien <ketsapiwiq@protonmail.com>
2024-09-27 14:58:29 +0200

    fix: adjust footer on click also

--- assets/arena.js
+++ assets/arena.js
@@ -1,18 +1,19 @@
 function() {
         
-    const footer = document.getElementById('send-area');
-    const modeScreen = document.getElementById('mode-screen');
-    const chatArea = document.getElementById('chat-area');
-  
+
     function adjustFooter() {
+      const footer = document.getElementById('send-area');
+      const modeScreen = document.getElementById('mode-screen');
+      const chatArea = document.getElementById('chat-area');
+    
       const footerHeight = footer.offsetHeight;
       // Add bottom padding to the chatArea equal to footer height so it's not hidden
       chatArea.style.paddingBottom = `${footerHeight}px`;
       modeScreen.style.paddingBottom = `${footerHeight}px`;
     }
-    // Adjust footer on page load, resize and initially
     window.addEventListener('load', adjustFooter);
     window.addEventListener('resize', adjustFooter);
+    window.addEventListener('click', adjustFooter);
     adjustFooter();
 
 }
