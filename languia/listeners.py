cee5d6d3b81ca4f59f2bc6b1a140157ff45102d9
Hadrien <ketsapiwiq@protonmail.com>
2024-10-15 14:32:49 +0200

    feat: better already visited mechanism

--- languia/listeners.py
+++ languia/listeners.py
@@ -114,19 +114,16 @@ def register_listeners():
         outputs=None,
         js="""(args) => {
 setTimeout(() => {
-document.getElementById("fr-modal-welcome-close").blur()
 
 const cookieExists = document.cookie.includes('comparia_already_visited');
-// Check if "already_visited" cookie exists
 if (!cookieExists) {
     document.cookie = 'name=comparia_already_visited; SameSite=None; Secure;'
-    console.log("First visit: Cookie created, modal stays open");
-} else {
     const modal = document.getElementById("fr-modal-welcome");
-    dsfr(modal).modal.conceal();
-    console.log("Cookie exists: Modal closed");
+    dsfr(modal).modal.disclose();
 }
+document.getElementById("fr-modal-welcome-close").blur();
 }, 500);
+
 }""",
     )
 
