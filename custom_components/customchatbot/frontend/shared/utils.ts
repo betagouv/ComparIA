f833e26aac1289735065719f969b580896c1e093
Hadrien <ketsapiwiq@protonmail.com>
2024-11-26 15:20:54 +0100

    comment

--- custom_components/customchatbot/frontend/shared/utils.ts
+++ custom_components/customchatbot/frontend/shared/utils.ts
@@ -115,6 +115,7 @@ export function update_messages(
 			index: i,
 			liked: message.liked !== undefined ? message.liked : false,
 			disliked: message.disliked !== undefined ? message.disliked : false,
+			// prefs: message.prefs !== undefined ? message.prefs : [],
 		  };
 		});
 	  }
@@ -133,8 +134,9 @@ export function update_messages(
 				type: "text",
 				index: i,
 
-				liked: message.liked !== undefined ? message.liked : oldMessage?.liked || false, // Merge liked status
-				disliked: message.disliked !== undefined ? message.disliked : oldMessage?.disliked || false, // Merge liked status
+				liked: message.liked !== undefined ? message.liked : oldMessage?.liked || false,
+				disliked: message.disliked !== undefined ? message.disliked : oldMessage?.disliked || false,
+				// prefs: message.prefs !== undefined ? message.prefs : oldMessage?.prefs || [],
 
 
 			};
