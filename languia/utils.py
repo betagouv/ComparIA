2a8a1c1b038c7071db9e3cf2fb278b148ebd2a4b
Hadrien <ketsapiwiq@protonmail.com>
2024-11-26 15:47:25 +0100

    fix: rstrip()

--- languia/utils.py
+++ languia/utils.py
@@ -614,9 +614,9 @@ def record_reaction(
     conversation_a_messages = messages_to_dict_list(conversations[0].messages)
     conversation_b_messages = messages_to_dict_list(conversations[1].messages)
 
-    if current_conversation.messages[msg_index].content.strip() != response_content:
+    if current_conversation.messages[msg_index].content.strip().rstrip() != response_content:
         logger.warning(
-            f"Incoherent content for liked message: '{response_content}' and '{current_conversation.messages[msg_index].content.strip()}'"
+            f"Incoherent content for liked message: '{response_content}' and '{current_conversation.messages[msg_index].content.strip().rstrip()}'"
         )
         logger.warning(f"Calculated index: {msg_index}")
 
