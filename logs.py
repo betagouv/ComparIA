1a1c52785b0b5d609f7bfa9f3285a0f21f309549
Hadrien <ketsapiwiq@protonmail.com>
2024-12-12 14:34:35 +0100

    wip: logs

---
+++ logs.py
@@ -0,0 +1,41 @@
+# def init_arene(session_hash: str, ip: str, cookie: str):
+#     return
+
+
+# def picked_models(self, model_a: str, model_b: str):
+#     return
+
+
+# def response_a(
+#     self,
+#     duration: float,
+#     length: int,
+#     content: str,
+#     endpoint_api: str,
+#     model_name: str,
+#     impact: str,
+# ):
+#     return
+
+
+# def response_b(
+#     self,
+#     duration: float,
+#     length: int,
+#     content: str,
+#     endpoint_api: str,
+#     model_name: str,
+#     impact: str,
+# ):
+#     return
+
+
+# def repicked_model(
+#     api_error: str,
+#     api_error_msg: str,
+#     old_model_name: str,
+#     model_name: str,
+# ):
+#     return
+
+# def msg_user():
