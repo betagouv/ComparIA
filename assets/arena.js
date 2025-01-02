e2b0aaf83889eceb9f3516849de79fac3d68ac3d
Hadrien <ketsapiwiq@protonmail.com>
2025-01-02 17:24:34 +0100

    wip: sentry frontend

--- assets/arena.js
+++ assets/arena.js
@@ -15,7 +15,25 @@ function() {
   window.addEventListener('click', adjustFooter);
   adjustFooter();
 
-  console.log("Git commit: __GIT_COMMIT__")
+  console.log("Git commit: __GIT_COMMIT__");
+
+  Sentry.onLoad(function () {
+    Sentry.init({
+      integrations: [
+        // If you use a bundle with tracing enabled, add the BrowserTracing integration
+        Sentry.browserTracingIntegration(),
+        // If you use a bundle with session replay enabled, add the Replay integration
+        Sentry.replayIntegration(),
+      ],
+
+      replaysSessionSampleRate: 0.1,
+      replaysOnErrorSampleRate: 1.0,
+      dsn: "__SENTRY_FRONT_DSN__",
+      environment: "__SENTRY_ENV__",
+
+      tracesSampleRate: 0.2
+    });
+  });
 
 }
 
