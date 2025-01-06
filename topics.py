a276f48e10d7b3f58b4ecf6c0b55112f4aa3c31f
Hadrien <ketsapiwiq@protonmail.com>
2025-01-06 17:12:55 +0100

    wip: topics

--- topics.py
+++ topics.py
@@ -1,10 +1,35 @@
 import json
-from ollama import chat
+from litellm import completion
 from pydantic import BaseModel
+from enum import Enum
 
-class Country(BaseModel):
-    name: str
-    capital: str
+class TXT360Category(str, Enum):
+    arts = "Arts"
+    business_economics_finance = "Business & Economics & Finance"
+    culture_cultural_geography = "Culture & Cultural geography"
+    daily_life_home_lifestyle = "Daily Life & Home & Lifestyle"
+    education = "Education"
+    entertainment_travel_hobby = "Entertainment & Travel & Hobby"
+    environment = "Environment"
+    food_drink_cooking = "Food & Drink & Cooking"
+    health_wellness_medicine = "Health & Wellness & Medicine"
+    law_justice = "Law & Justice"
+    natural_science_formal_science_technology = "Natural Science & Formal Science & Technology"
+    personal_development_human_resources_career = "Personal Development & Human Resources & Career"
+    politics_government = "Politics & Government"
+    religion_spirituality = "Religion & Spirituality"
+    shopping_commodity = "Shopping & Commodity"
+    society_social_issues_human_rights = "Society & Social Issues & Human Rights"
+    sports = "Sports"
+    other = "Other"
+
+class SumUp(BaseModel):
+    short_summary: str
+    keywords: list[str]
+    # lmarena_categories: list[str]
+
+    txt360_categories: list[TXT360Category]
+
     languages: list[str]
 
 # Read the .json file and extract the 'conv_a' JSON object
@@ -20,21 +45,29 @@ if not conv_a:
 # Use the content from 'conv_a' to formulate a query
 query_content = f"Based on the conversation: {conv_a}, provide some keywords."
 
-# Call the LLM using the Ollama chat API
-response = chat(
+#   //   "model_id": "llama-3.1-70b",
+#   //   "api_id": "albert-llama-3.1-70b",
+#   //   "api_base": "https://albert.api.etalab.gouv.fr/v1/",
+#   //   "api_type": "openai",
+#   //   "api_key": "langia-",
+#   //   "model_name": "meta-llama/Meta-Llama-3.1-70B-Instruct",
+#   //   "recommended_config": {
+#   //     "temperature": 0.7,
+response = completion(
     messages=[
         {
             'role': 'user',
             'content': query_content,
         }
     ],
+    
     model='llama3.1',
-    format=Country.model_json_schema(),
+    format=SumUp.model_json_schema(),
 )
 
 # Parse the structured response
 try:
-    country = Country.model_validate_json(response.message.content)
-    print(country)
+    sum_up = SumUp.model_validate_json(response.message.content)
+    print(sum_up)
 except Exception as e:
     print("Failed to parse the response:", e)
