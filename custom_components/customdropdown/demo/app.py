9b3c65c10c5267322aeba26bf851e2df3f86cd44
Hadrien <ketsapiwiq@protonmail.com>
2025-01-14 14:36:59 +0100

    cc: add customdropdown

---
+++ custom_components/customdropdown/demo/app.py
@@ -0,0 +1,17 @@
+
+import gradio as gr
+from gradio_customdropdown import CustomDropdown
+
+
+example = CustomDropdown().example_value()
+
+demo = gr.Interface(
+    lambda x:x,
+    CustomDropdown(),  # interactive version of your component
+    CustomDropdown(),  # static version of your component
+    # examples=[[example]],  # uncomment this line to view the "example version" of your component
+)
+
+
+if __name__ == "__main__":
+    demo.launch()
