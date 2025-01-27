a841fed0244aafda30192600595b1a327f1377c3
Hadrien <ketsapiwiq@protonmail.com>
2025-01-27 13:52:06 +0100

    fix: offset style textarea

--- custom_components/customdropdown/frontend/shared/Textbox.svelte
+++ custom_components/customdropdown/frontend/shared/Textbox.svelte
@@ -238,6 +238,8 @@
 		/* border: 1px 1px 0 1px solid #e5e5e5 !important; */
 		/* min-height: 4rem !important; */
 		box-shadow: inset 0 -2px 0 0 #6a6af4 !important;
+		outline: none !important;
+		outline-offset: 0 !important;
 	}
 
 	@media (min-width: 48em) {
