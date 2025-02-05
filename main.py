4bbc88d5a415f8ae35edf131016ace300b553e17
Hadrien <ketsapiwiq@protonmail.com>
2025-02-05 12:29:17 +0100

    feat: share feature

--- main.py
+++ main.py
@@ -103,6 +103,92 @@ async def models(request: Request):
     )
 
 
+@app.get("/share", response_class=HTMLResponse)
+async def share(i: str, request: Request):
+
+    from languia.config import all_models_extra_info_toml
+    try:
+        import base64, json
+        decoded = base64.b64decode(i)
+        print(decoded)
+        data = json.loads(decoded)
+        print(data)
+        print(all_models_extra_info_toml)
+        assert data.get("a") in all_models_extra_info_toml
+        model_a_name = data.get("a")
+        print(model_a_name)
+        assert data.get("b") in all_models_extra_info_toml
+        model_b_name = data.get("b")
+        print(model_b_name)
+        assert isinstance(data.get("ta"), int)
+        model_a_tokens = data.get("ta")
+        assert isinstance(data.get("tb"), int)
+        model_b_tokens = data.get("tb")
+        assert (data.get("c") in ['a','b'] or data.get("c") == None)
+        if data.get("c") == "a":
+            chosen_model = "model-a"
+        elif data.get("c") == "b":
+            chosen_model = "model-b"
+        else:
+            chosen_model = None
+    except:
+        return FileResponse("templates/50x.html", status_code=500)
+
+    from languia.utils import get_model_extra_info
+    from languia.config import models_extra_info
+
+    model_a = get_model_extra_info(model_a_name, models_extra_info)
+    model_b = get_model_extra_info(model_b_name, models_extra_info)
+
+    from languia.reveal import (
+        get_llm_impact,
+        calculate_lightbulb_consumption,
+        calculate_streaming_hours,
+    )
+
+    model_a_impact = get_llm_impact(model_a, model_a_name, model_a_tokens, None)
+    model_b_impact = get_llm_impact(model_b, model_b_name, model_b_tokens, None)
+
+    lightbulb_a, lightbulb_a_unit = calculate_lightbulb_consumption(
+        model_a_impact.energy.value
+    )
+    lightbulb_b, lightbulb_b_unit = calculate_lightbulb_consumption(
+        model_b_impact.energy.value
+    )
+
+    streaming_a, streaming_a_unit = calculate_streaming_hours(model_a_impact.gwp.value)
+    streaming_b, streaming_b_unit = calculate_streaming_hours(model_b_impact.gwp.value)
+
+    return templates.TemplateResponse(
+        "share.html",
+        {
+            "title": "Mon bilan",
+            "model_a": model_a,
+            "model_b": model_b,
+            "chosen_model": chosen_model,
+            "model_a_impact": model_a_impact,
+            "model_b_impact": model_b_impact,
+            "size_desc": size_desc,
+            "license_desc": license_desc,
+            "license_attrs": license_attrs,
+            "model_a_tokens": model_a_tokens,
+            "model_b_tokens": model_b_tokens,
+            "streaming_a": streaming_a,
+            "streaming_a_unit": streaming_a_unit,
+            "streaming_b": streaming_b,
+            "streaming_b_unit": streaming_b_unit,
+            "lightbulb_a": lightbulb_a,
+            "lightbulb_a_unit": lightbulb_a_unit,
+            "lightbulb_b": lightbulb_b,
+            "lightbulb_b_unit": lightbulb_b_unit,
+            "request": request,
+            "config": config,
+            "models": config.models_extra_info,
+            "size_desc": size_desc,
+            "license_desc": license_desc,
+            "license_attrs": license_attrs,
+        },
+    )
 
 
 @app.get("/a-propos", response_class=HTMLResponse)
