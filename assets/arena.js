c45422c37d7693a531b0c15f680cfe9b49c2ab97
Hadrien <ketsapiwiq@protonmail.com>
2025-04-08 18:13:23 +0200

    fix: mode screen doesn't have footer

--- assets/arena.js
+++ assets/arena.js
@@ -47,13 +47,11 @@ function() {
 
   function adjustFooter() {
     const footer = document.getElementById('send-area');
-    const modeScreen = document.getElementById('mode-screen');
     const chatArea = document.getElementById('chat-area');
 
     const footerHeight = footer.offsetHeight;
     // Add bottom padding to the chatArea equal to footer height so it's not hidden
     chatArea.style.paddingBottom = `${footerHeight}px`;
-    modeScreen.style.paddingBottom = `${footerHeight}px`;
   }
   window.addEventListener('load', adjustFooter);
   window.addEventListener('resize', adjustFooter);
