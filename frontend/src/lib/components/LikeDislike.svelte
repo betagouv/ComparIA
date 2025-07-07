32238071722c9b32d04fc0fd01f78f3db0921eef
axolotle <nchesnais@mailoo.org>
2025-07-07 17:04:20 +0200

    refactor(front): update LikeDislike

--- frontend/src/lib/components/LikeDislike.svelte
+++ frontend/src/lib/components/LikeDislike.svelte
@@ -7,7 +7,7 @@
   import ThumbUpDefault from '$lib/icons/ThumbUpDefault.svelte'
   import ThumbUpDisabled from '$lib/icons/ThumbUpDisabled.svelte'
 
-  export let handle_action: (selected: string | null) => void
+  export let onLikeDislikeSelected: (selected: 'like' | 'dislike' | null) => void
   export let disabled = false
 
   let selected: 'like' | 'dislike' | null = null
@@ -22,10 +22,10 @@
   on:click={() => {
     if (selected === 'like') {
       selected = null // Unselect the "like"
-      handle_action('none') // Notify that no action is selected
+      onLikeDislikeSelected(selected) // Notify that no action is selected
     } else {
       selected = 'like' // Select the "like"
-      handle_action('like') // Notify that "like" was selected
+      onLikeDislikeSelected(selected) // Notify that "like" was selected
     }
   }}
 />
@@ -39,10 +39,10 @@
   on:click={() => {
     if (selected === 'dislike') {
       selected = null // Unselect the "dislike"
-      handle_action('none') // Notify that no action is selected
+      onLikeDislikeSelected(selected) // Notify that no action is selected
     } else {
       selected = 'dislike' // Select the "dislike"
-      handle_action('dislike') // Notify that "dislike" was selected
+      onLikeDislikeSelected(selected) // Notify that "dislike" was selected
     }
   }}
 />
