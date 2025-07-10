a53f9d269dc594249cb7ab5954bfd8b7def1d80b
axolotle <nchesnais@mailoo.org>
2025-07-10 14:47:41 +0200

    temp: disable call to api

--- frontend/src/routes/arene/+page.server.ts
+++ frontend/src/routes/arene/+page.server.ts
@@ -2,6 +2,7 @@ import { api } from "$lib/api";
 import type { Model } from "$lib/chatService.svelte";
 
 export async function load() {
-    const result = await api.predict('/enter_arena')
-	return { models: (result.data as [Model[]])[0] }
+    // const result = await api.predict('/enter_arena')
+	// return { models: (result.data as [Model[]])[0] }
+    return { models: [] }
 }
