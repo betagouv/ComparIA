731f87a10ddddb73c8ee953f8c42aa1be35d2d94
axolotle <nchesnais@mailoo.org>
2025-07-16 12:27:18 +0200

    feat(front): add i18n helpers

--- frontend/src/lib/utils/commons.ts
+++ frontend/src/lib/utils/commons.ts
@@ -1,1 +1,20 @@
+import { sanitize as gradioSanitize } from '@gradio/sanitize'
+
 export const noop = () => {}
+
+export function sanitize(html: string): string {
+  return gradioSanitize(html, '/')
+}
+
+export function propsToAttrs(props: Record<string, unknown>): string {
+  return Object.entries(props)
+    .map(([k, v]) => `${k}="${v}"`)
+    .join(' ')
+}
+
+export function externalLinkProps(props: Record<string, unknown> | string): string {
+  const _props = { target: '_blank', rel: 'noopener external' }
+  return propsToAttrs(
+    typeof props === 'string' ? { ..._props, href: props } : { ..._props, ...props }
+  )
+}
