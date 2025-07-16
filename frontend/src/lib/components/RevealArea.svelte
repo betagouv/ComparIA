1ee0d29087aa046b72c57d783ba477a7fcabec94
axolotle <nchesnais@mailoo.org>
2025-07-16 10:37:45 +0200

    feat(front): update RevealArea with feedback and footer

--- frontend/src/lib/components/RevealArea.svelte
+++ frontend/src/lib/components/RevealArea.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
   import type { RevealData } from '$lib/chatService.svelte'
+  import Footer from '$lib/components/Footer.svelte'
   import ModelInfoModal from '$lib/components/ModelInfoModal.svelte'
 
   let { data }: { data: RevealData } = $props()
 
   const { selected, modelsData, shareB64Data, ...infos } = data
+
+  let shareInput: HTMLInputElement
+  // FIXME refactor into component
+  let snackbar: HTMLElement
+
+  function copyShareLink() {
+    shareInput.select()
+    navigator.clipboard.writeText(shareInput.value)
+    createSnackbar('Lien copié dans le presse-papiers')
+  }
+
+  function createSnackbar(message: string) {
+    const messageText = snackbar.querySelector('.message')
+    messageText!.textContent = message
+
+    snackbar.classList.add('show')
+
+    setTimeout(() => {
+      snackbar.classList.remove('show')
+    }, 2000)
+  }
+
+  function closeSnackbar() {
+    snackbar.classList.remove('show')
+  }
 </script>
 
 <div id="reveal-screen" class="fr-pt-4w next-screen">
@@ -301,4 +327,179 @@
       {/each}
     </div>
   </div>
+
+  <div class="feedback">
+    <div id="feedback-row">
+      <div class="fr-container fr-mb-4w text-center">
+        <a class="btn fr-btn--secondary fr-my-2w feedback-btns" href="/" target="_blank">
+          Revenir à l'accueil
+        </a><br />
+        <button
+          class="btn fr-btn--secondary fr-my-2w feedback-btns"
+          data-fr-opened="false"
+          aria-controls="share-modal"
+        >
+          <svg
+            class="inline"
+            width="21"
+            height="20"
+            viewBox="0 0 21 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10.5 2.15479L15.6726 7.32737L14.4941 8.50585L11.3333 5.34513V13.3333H9.66667V5.34513L6.50592 8.50585L5.32741 7.32737L10.5 2.15479ZM3 14.9999V11.6666H4.66667V14.9999C4.66667 15.4602 5.03977 15.8333 5.5 15.8333H15.5C15.9602 15.8333 16.3333 15.4602 16.3333 14.9999V11.6666H18V14.9999C18 16.3807 16.8807 17.4999 15.5 17.4999H5.5C4.11929 17.4999 3 16.3807 3 14.9999Z"
+              fill="#6A6AF4"
+            />
+          </svg>&nbsp;Partager le résultat
+        </button><br />
+        <!-- Remplacer par https://monitor.bunka.ai/compar:ia ? -->
+        <a
+          class="fr-mx-auto fr-mb-4w link"
+          href="https://languia-metabase.stg.cloud.culture.fr/public/dashboard/7dde3be2-6680-49ac-966b-ade9ad36dfcf?tab=29-tableau-1"
+          target="_blank">En savoir plus sur les votes</a
+        >
+      </div>
+    </div>
+
+    <dialog
+      aria-labelledby="fr-modal-title-share-modal"
+      role="dialog"
+      id="share-modal"
+      class="fr-modal"
+    >
+      <div class="fr-container fr-container--fluid fr-container-md">
+        <div class="fr-grid-row fr-grid-row--center">
+          <div class="fr-col-12 fr-col-md-8 fr-col-lg-6">
+            <div class="fr-modal__body">
+              <div class="fr-modal__header">
+                <button
+                  class="fr-btn--close fr-btn"
+                  title="Fermer la fenêtre modale"
+                  aria-controls="share-modal">Fermer</button
+                >
+              </div>
+              <div class="fr-modal__content">
+                <h6 class="fr-text--lg">Partagez votre résultat</h6>
+                <p class="fr-text-md--sm fr-text--xs">
+                  Faites découvrir compar:IA en partageant les modèles d’IA avec lesquels vous avez
+                  échangé ! Seuls les noms et l’impact énergétique de la discussion seront visibles
+                  via ce lien, sans accès aux messages échangés.
+                </p>
+                <div class="fr-grid-row fr-mb-4w">
+                  <input
+                    bind:this={shareInput}
+                    type="text"
+                    id="share-link"
+                    class="fr-col-md-8 fr-col-12 fr-input inline"
+                    value="https://www.comparia.beta.gouv.fr/share?i={shareB64Data}"
+                  />
+                  <button class="fr-col-md-4 fr-col-12 btn purple-btn block" onclick={copyShareLink}
+                    ><svg
+                      width="15"
+                      height="16"
+                      viewBox="0 0 15 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="inline"
+                    >
+                      <path
+                        d="M8.29571 5.08317L9.35636 6.14383C11.4066 8.19412 11.4066 11.5182 9.35636 13.5685L9.09124 13.8336C7.04096 15.8839 3.71685 15.8839 1.6666 13.8336C-0.383657 11.7834 -0.383657 8.45924 1.6666 6.409L2.72726 7.46969C1.2628 8.93414 1.2628 11.3085 2.72726 12.7729C4.19173 14.2374 6.56606 14.2374 8.03059 12.7729L8.29571 12.5078C9.76016 11.0433 9.76016 8.66894 8.29571 7.20449L7.23506 6.14383L8.29571 5.08317ZM13.3338 9.59099L12.2732 8.53034C13.7376 7.06582 13.7376 4.69148 12.2732 3.22702C10.8087 1.76255 8.43439 1.76255 6.96994 3.22702L6.70474 3.49218C5.24027 4.95664 5.24027 7.33102 6.70474 8.79547L7.76539 9.85612L6.70474 10.9168L5.64407 9.85612C3.59382 7.80592 3.59382 4.48177 5.64407 2.43152L5.90924 2.16635C7.95949 0.116099 11.2836 0.116099 13.3338 2.16635C15.3841 4.2166 15.3841 7.54072 13.3338 9.59099Z"
+                        fill="white"
+                      />
+                    </svg>&nbsp;Copier le lien</button
+                  >
+                </div>
+                <img
+                  class="fr-responsive-img fr-mb-4w"
+                  src="../assets/share-example.png"
+                  alt="Exemple de partage de résultat"
+                  title="Exemple de partage de résultat"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </dialog>
+
+    <div id="snackbar" bind:this={snackbar}>
+      <div class="checkmark">
+        <svg
+          width="16"
+          height="15"
+          viewBox="0 0 16 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M8.25477 13.7224C4.91029 13.7224 2.19922 11.0114 2.19922 7.66688C2.19922 4.3224 4.91029 1.61133 8.25477 1.61133C11.5993 1.61133 14.3103 4.3224 14.3103 7.66688C14.3103 11.0114 11.5993 13.7224 8.25477 13.7224ZM7.65104 10.0891L11.9323 5.80722L11.0761 4.95097L7.65104 8.3766L5.93792 6.66348L5.08166 7.51973L7.65104 10.0891Z"
+            fill="white"
+          />
+        </svg>
+      </div>
+      <span class="message"></span><span class="close" onclick={closeSnackbar()}
+        ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12">
+          <path
+            d="m12 10.6 4.95-4.96 1.4 1.4L13.42 12l4.96 4.95-1.4 1.4L12 13.42l-4.95 4.96-1.4-1.4L10.58 12 5.63 7.05l1.4-1.4z"
+          />
+        </svg></span
+      >
+    </div>
+  </div>
 </div>
+
+<Footer />
+
+<style>
+  #snackbar {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    border: 2px solid #6a6af4;
+    background-color: white;
+    color: #333;
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: stretch;
+    /* just enough! */
+    z-index: 1750;
+    visibility: hidden;
+    opacity: 0;
+    transition:
+      opacity 0.3s ease-in-out,
+      visibility 0s linear 0.3s;
+  }
+
+  :global(#snackbar.show) {
+    visibility: visible;
+    opacity: 1;
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  #snackbar .checkmark {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+    background-color: #6a6af4;
+  }
+
+  #snackbar .message {
+    align-items: center;
+    display: flex;
+    padding: 0 20px;
+  }
+
+  #snackbar .close {
+    margin-right: 10px;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    cursor: pointer;
+    color: #333;
+  }
+</style>
