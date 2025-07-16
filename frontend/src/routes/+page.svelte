b645092cf1805c62b350e7ad7f5f5d2ca9fd6354
Hadrien <ketsapiwiq@protonmail.com>
2025-07-16 14:19:13 +0200

    chore(front): switch to adapter-static

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  let { data } = $props()
+
+  onMount(async () => {
+    // FIXME import only modal? Or create custom component
+    // @ts-ignore - DSFR module import
+    await import('@gouvfr/dsfr/dist/dsfr/dsfr.module.min.js')
+  })
+</script>
+
+<h1>Bienvenue.</h1>
