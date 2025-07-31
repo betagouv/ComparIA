d60b11b7647fcdfd7529411011cc42240b673e98
axolotle <nchesnais@mailoo.org>
2025-07-31 16:07:31 +0200

    feat(front): add LanguageSelector component and state

---
+++ frontend/src/lib/components/header/LanguageSelector.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { LOCALES, changeLocale, global } from '$lib/global.svelte'
+
+  let { id }: { id: string } = $props()
+</script>
+
+<nav class="fr-translate fr-nav">
+  <div class="fr-nav__item">
+    <button
+      aria-controls={id}
+      aria-expanded="false"
+      title="Sélectionner une langue"
+      type="button"
+      class="fr-translate__btn fr-btn fr-btn--tertiary"
+    >
+      {LOCALES.find((locale) => locale.code === global.locale)!.short}
+    </button>
+    <div class="fr-collapse fr-translate__menu fr-menu" {id}>
+      <ul class="fr-menu__list">
+        {#each LOCALES as locale}
+          <li>
+            <button
+              class="fr-translate__language fr-nav__link"
+              lang={locale.code}
+              aria-current={locale.code == global.locale}
+              onclick={() => changeLocale(locale.code)}
+            >
+              {locale.long}
+            </button>
+          </li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</nav>
--- frontend/src/lib/global.svelte.ts
+++ frontend/src/lib/global.svelte.ts
@@ -1,13 +1,26 @@
 import { api } from '$lib/api'
+import { getLocale, setLocale, type Locale } from '$lib/i18n/runtime'
 
 export type State = {
   loading: boolean
+  locale: Locale
   votes?: { count: number; objective: number }
 }
 
+export const LOCALES = [
+  { code: 'fr', short: 'FR', long: 'FR - Français' },
+  { code: 'en', short: 'EN', long: 'EN - English' }
+] as const
+
 export const global = $state<State>({
-  loading: false
+  loading: false,
+  locale: getLocale()
 })
+
+export function changeLocale(locale: Locale) {
+  setLocale(locale)
+  global.locale = locale
+}
 
 export function getVotes() {
   return api.get<State['votes']>('/counter').then((data) => {
