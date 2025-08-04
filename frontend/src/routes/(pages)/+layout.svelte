5da440311fa324fd946cb0d067896d229948b0ae
axolotle <nchesnais@mailoo.org>
2025-08-04 14:40:39 +0200

    refactor(front): update regular layout (banner + gauge)

--- frontend/src/routes/(pages)/+layout.svelte
+++ frontend/src/routes/(pages)/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { page } from '$app/state'
+  import { Link } from '$lib/components/dsfr'
   import Footer from '$lib/components/Footer.svelte'
   import { Header, VoteGauge } from '$lib/components/header'
+  import { m } from '$lib/i18n/messages'
 
   let { children } = $props()
 
@@ -11,40 +13,15 @@
 <Header hideDiscussBtn={isHome} />
 
 {#if isHome}
-  <div>
-    <a class="temp-banner text-center" href="/datasets">
-      Découvrez l’étude de Bunka.ai qui explore les questions posées sur compar:IA !
-    </a>
-  </div>
+  <Link button href="#european" text={m['header.banner']()} cornered class="w-auto!" />
 {/if}
 
-<div class="fr-container--fluid gauge-banner flex items-center justify-center lg:hidden">
+<div
+  class="fr-container--fluid bg-light-grey drop-shadow-(--raised-shadow) flex h-[48px] items-center justify-center lg:hidden"
+>
   <VoteGauge id="mobile-vote-gauge" />
 </div>
 
 {@render children()}
 
 <Footer />
-
-<style>
-  .gauge-banner {
-    gap: 1em;
-    height: 48px;
-    background-color: #f3f5f9;
-    --shadow-color: rgba(0, 0, 18, 0.16);
-    --raised-shadow: 0 1px 3px var(--shadow-color);
-    filter: drop-shadow(0 1px 3px var(--shadow-color));
-  }
-
-  a.temp-banner,
-  a.temp-banner:hover,
-  a.temp-banner:active {
-    display: block;
-    background-color: #6a6af4 !important;
-    color: white !important;
-    line-height: 1.5em;
-    padding-top: 0.5em;
-    padding-bottom: 0.5em;
-    --underline-img: none;
-  }
-</style>
