99b4510f8bf0562ee808dca0713aa583067f32b1
axolotle <nchesnais@mailoo.org>
2025-08-06 09:30:19 +0200

    feat(front): add product page's param handling and seo title

---
+++ frontend/src/routes/(pages)/product/[tab]/+page.server.ts
@@ -0,0 +1,14 @@
+import { error } from '@sveltejs/kit'
+
+export function load({ params }) {
+  const TABS = ['comparator', 'problem', 'history', 'faq', 'partners'] as const
+  type TabsId = (typeof TABS)[number]
+
+  if (!TABS.includes(params.tab as TabsId)) {
+    error(404)
+  }
+
+  return {
+    tab: params.tab as TabsId
+  }
+}
--- frontend/src/routes/(pages)/product/[tab]/+page.svelte
+++ frontend/src/routes/(pages)/product/[tab]/+page.svelte
@@ -1,9 +1,9 @@
-<script>
-  import About from './About.svelte'
-  import FAQ from './FAQ.svelte'
-  import Partners from './Partners.svelte'
+<script lang="ts">
+  import SeoHead from '$components/SEOHead.svelte'
+  import { m } from '$lib/i18n/messages'
+  import { Comparator, FAQ, History, Partners, Problem } from './components'
+
+  const { data } = $props()
 </script>
 
-<About />
-<Partners />
-<FAQ />
+<SeoHead title={m[`seo.titles.${data.tab}`]()} />
