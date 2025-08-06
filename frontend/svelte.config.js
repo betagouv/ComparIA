47d04d5931a2fc2a1b0bf965053c511b2260b0d3
axolotle <nchesnais@mailoo.org>
2025-08-06 08:47:15 +0200

    chore(front): add custom path alias to svelte config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,7 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 const config = {
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter() }
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $css: './src/css',
+      $components: './src/lib/components'
+    }
+  }
 }
 
 export default config
