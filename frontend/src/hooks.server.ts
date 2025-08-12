0ca78b491d8ae7bf16f162ba314c30d169282613
axolotle <nchesnais@mailoo.org>
2025-08-12 11:06:52 +0200

    fix(front): fix dark mode flickering bis

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -5,9 +5,13 @@ import type { Handle } from '@sveltejs/kit'
 const paraglideHandle: Handle = ({ event, resolve }) =>
   paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
     event.request = localizedRequest
+
     return resolve(event, {
       transformPageChunk: ({ html }) => {
-        return html.replaceAll('%lang%', locale)
+        return html
+          .replaceAll('%lang%', locale)
+          .replace('%scheme%', event.cookies.get('scheme') || 'system')
+          .replace('%theme%', event.cookies.get('theme') || 'system')
       }
     })
   })
