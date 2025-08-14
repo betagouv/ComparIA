3f004516ae6ceb263daf0931afc5058f28c8a674
axolotle <nchesnais@mailoo.org>
2025-08-14 15:37:24 +0200

    feat(front): add AccordionGroup component

---
+++ frontend/src/lib/components/dsfr/AccordionGroup.svelte
@@ -0,0 +1,16 @@
+<script lang="ts" generics="T extends {id?: string; label: string; content: string}">
+  import type { SvelteHTMLElements } from 'svelte/elements'
+  import Accordion from './Accordion.svelte'
+
+  let { items, children, ...props }: { items?: T[] } & SvelteHTMLElements['div'] = $props()
+</script>
+
+<div data-fr-group="true" class="fr-accordions-group" {...props}>
+  {#if items}
+    {#each items as item, i}
+      <Accordion id={item.id ?? i.toString()} label={item.label} content={item.content} />
+    {/each}
+  {:else}
+    {@render children?.()}
+  {/if}
+</div>
