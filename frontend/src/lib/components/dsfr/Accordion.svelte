f25378b1b111a9259a18aa9c3569fae7e3dae425
axolotle <nchesnais@mailoo.org>
2025-08-14 15:37:01 +0200

    feat(front):  add Accordion component

---
+++ frontend/src/lib/components/dsfr/Accordion.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { SvelteHTMLElements } from 'svelte/elements'
+
+  let {
+    id,
+    label,
+    content,
+    children,
+    ...props
+  }: {
+    id: string
+    label: string
+    content?: string
+  } & SvelteHTMLElements['section'] = $props()
+</script>
+
+<section class="fr-accordion" {...props}>
+  <h3 class="fr-accordion__title">
+    <button
+      type="button"
+      class="fr-accordion__btn"
+      aria-expanded="false"
+      aria-controls={`accordion-${id}`}
+    >
+      {label}
+    </button>
+  </h3>
+  <div class="fr-collapse" id={`accordion-${id}`}>
+    {#if content}{content}{:else}{@render children?.()}{/if}
+  </div>
+</section>
