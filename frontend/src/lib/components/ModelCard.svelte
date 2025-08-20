7d57ac831953398b770a3646a964338637bd8c9b
axolotle <nchesnais@mailoo.org>
2025-08-20 18:37:25 +0200

    refactor(front): rework ModelCard

--- frontend/src/lib/components/ModelCard.svelte
+++ frontend/src/lib/components/ModelCard.svelte
@@ -1,40 +1,29 @@
 <script lang="ts">
-  import type { APIBotModel } from '$lib/models'
+  import type { BotModel } from '$lib/models'
+  import { sanitize } from '$lib/utils/commons'
+  import { Badge } from './dsfr'
 
   let {
     model,
     onModelSelected,
     modalId
   }: {
-    model: APIBotModel
+    model: BotModel
     onModelSelected: (name: string) => void
     modalId: string
   } = $props()
 
-  const badges = [
-    model.fully_open_source
-      ? { color: 'green-emeraude', text: 'Open source' }
-      : model.distribution === 'open-weights'
-        ? { color: 'yellow-tournesol', text: 'Semi-ouvert' }
-        : { color: 'orange-terre-battue', text: 'Propriétaire' },
-    model.release_date ? { color: '', text: `Sortie ${model.release_date}` } : null,
-    {
-      color: 'info',
-      text:
-        model.distribution === 'open-weights'
-          ? `${Math.round(model.params)} mds de paramètres`
-          : `Taille estimée (${model.friendly_size})`
-    }
-  ].filter((b) => !!b)
+  const { license, releaseDate, size } = model.badges
+  const badges = [license, releaseDate, size].filter((b) => !!b)
 </script>
 
 <div class="fr-card fr-enlarge-link cg-border bg-none! rounded-xl">
   <div class="fr-card__body">
     <div class="fr-card__content px-5! md:px-4! md:pt-4!">
-      <h6 class="fr-card__title text-dark-grey font-normal! text-sm! mb-3! flex items-start gap-3">
+      <h6 class="fr-card__title text-dark-grey font-normal! text-sm! mb-3! flex items-center gap-3">
         <img
           class="object-contain"
-          src="/orgs/{model.icon_path}"
+          src="/orgs/ai/{model.icon_path}"
           width="20"
           alt="{model.organisation} logo"
         />
@@ -50,16 +39,14 @@
         </div>
       </h6>
 
-      <p class="fr-card__desc">{model.excerpt}</p>
+      <div class="fr-card__desc">
+        {@html sanitize(model.desc).replaceAll('<p>', '<p class="last:mb-0!">')}
+      </div>
 
       <div class="fr-card__start order-2!">
         <ul class="fr-badges-group">
-          {#each badges as badge}
-            <li>
-              <p class={`text-xs! fr-badge fr-badge--${badge.color} fr-badge--no-icon`}>
-                {badge.text}
-              </p>
-            </li>
+          {#each badges as badge, i}
+            <li><Badge id="card-badge-{i}" size="xs" {...badge} noTooltip /></li>
           {/each}
         </ul>
       </div>
