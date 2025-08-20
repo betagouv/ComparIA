e6356b631fc918aa469dbda145dd6b4a021ede6d
axolotle <nchesnais@mailoo.org>
2025-08-20 17:41:08 +0200

    refactor(front): add xs size + new colors to Badge

--- frontend/src/lib/components/dsfr/Badge.svelte
+++ frontend/src/lib/components/dsfr/Badge.svelte
@@ -3,22 +3,33 @@
   import type { SvelteHTMLElements } from 'svelte/elements'
 
   export type BadgeProps = {
-    id: string
+    id?: string
     size?: 'xs' | 'sm' | 'md'
-    variant?: '' | 'info' | 'green' | 'orange' | 'yellow'
+    variant?: '' | 'info' | 'green' | 'orange' | 'yellow' | 'purple' | 'red'
     text?: string
     tooltip?: string
+    noTooltip?: boolean
   } & SvelteHTMLElements['span']
 
-  let { id, variant = '', text, size = 'md', tooltip, children, ...props }: BadgeProps = $props()
+  let {
+    id,
+    variant = '',
+    text,
+    size = 'md',
+    tooltip,
+    noTooltip = false,
+    children,
+    ...props
+  }: BadgeProps = $props()
 
   const variants = {
     '': '',
     info: 'info',
     green: 'green-emeraude',
     orange: 'orange-terre-battue',
-    yellow: 'yellow-tournesol'
-    // FIXME red
+    yellow: 'yellow-tournesol',
+    purple: 'purple-glycine',
+    red: 'error'
   } as const
 </script>
 
@@ -27,12 +38,13 @@
   {id}
   class={[
     `fr-badge fr-badge--${variants[variant]} fr-badge--${size} fr-badge--no-icon`,
+    { 'text-xs!': size === 'xs' },
     props.class
   ]}
 >
   {#if typeof text === 'string'}
     {text}
-    {#if tooltip}
+    {#if tooltip && !noTooltip}
       <Tooltip id="{id}-tooltip" size="xs" text={tooltip} class="ms-1" />
     {/if}
   {:else}
