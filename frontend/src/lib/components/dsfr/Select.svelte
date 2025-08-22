9ab8a316ae3eb82f7d2eab7cd196f4cc5c38ca51
axolotle <nchesnais@mailoo.org>
2025-08-22 18:56:54 +0200

    feat(front): add Select component

---
+++ frontend/src/lib/components/dsfr/Select.svelte
@@ -0,0 +1,35 @@
+<script
+  lang="ts"
+  generics="Value extends number | string, Option extends { value: Value, label: string }"
+>
+  import type { HTMLSelectAttributes } from 'svelte/elements'
+
+  type SelectProps = {
+    id: string
+    selected: Value
+    label: string
+    options: Option[]
+    hideLabel?: boolean
+  } & HTMLSelectAttributes
+
+  let {
+    id,
+    selected = $bindable(),
+    label,
+    options,
+    hideLabel = false,
+    ...props
+  }: SelectProps = $props()
+</script>
+
+<div class="fr-select-group">
+  <label class={['fr-label', { 'fr-sr-only': hideLabel }]} for={id}>
+    {label}
+  </label>
+
+  <select {...props} bind:value={selected} class="fr-select" {id} name="table-footer-select-7847">
+    {#each options as option}
+      <option value={option.value}>{option.label}</option>
+    {/each}
+  </select>
+</div>
