29d88116c0e6f0d4afe3d3c907bd99e28137cd0e
axolotle <nchesnais@mailoo.org>
2025-08-24 12:39:21 +0200

    feat(front): add ranking Table

--- frontend/src/routes/(pages)/ranking/+page.svelte
+++ frontend/src/routes/(pages)/ranking/+page.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
+  import { Table } from '$components/dsfr'
   import SeoHead from '$lib/components/SEOHead.svelte'
   import { m } from '$lib/i18n/messages'
+  import { getModelsContext } from '$lib/models'
   import { propsToAttrs, sanitize } from '$lib/utils/commons'
+
+  const modelsData = getModelsContext()
+
+  const cols = (
+    [
+      { id: 'rank' },
+      { id: 'name', orderable: true },
+      { id: 'elo', orderable: true, tooltip: 'FIXME' },
+      { id: 'trust', tooltip: 'FIXME' },
+      { id: 'votes', orderable: true },
+      { id: 'consumption', orderable: true },
+      { id: 'size', orderable: true, tooltip: 'FIXME' },
+      { id: 'release', orderable: true },
+      { id: 'organisation', orderable: true },
+      { id: 'license' }
+    ] as const
+  ).map((col) => ({
+    ...col,
+    label: m[`ranking.table.data.cols.${col.id}`]()
+  }))
+
+  let orderingCol = $state<(typeof cols)[number]['id']>('elo')
 </script>
 
 <SeoHead title={m['seo.titles.ranking']()} />
@@ -17,5 +41,11 @@
         })
       )}
     </p>
+
+    <Table caption={m['ranking.title']()} {cols} rows={modelsData} bind:orderingCol hideCaption>
+      {#snippet cell(model, col)}
+        {model[col.id]}
+      {/snippet}
+    </Table>
   </div>
 </main>
