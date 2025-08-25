2e3e418ad3903382339d8daf3107e07621e67442
axolotle <nchesnais@mailoo.org>
2025-08-25 11:17:58 +0200

    feat(front): add Search component

---
+++ frontend/src/lib/components/dsfr/Search.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { m } from '$lib/i18n/messages'
+  import type { HTMLInputAttributes } from 'svelte/elements'
+  import Button from './Button.svelte'
+
+  let {
+    id,
+    value = $bindable(),
+    label,
+    placeholder = label,
+    class: classes,
+    ...props
+  }: {
+    id: string
+    value: string
+    label: string
+  } & HTMLInputAttributes = $props()
+</script>
+
+<div class={['fr-search-bar', classes]} role="search">
+  <label class="fr-label" for={id}>{label}</label>
+  <input bind:value {...props} class="fr-input" {placeholder} {id} type="search" />
+  <Button text={m['words.search']()} title={m['words.search']()} iconOnly />
+</div>
+
+<style lang="postcss">
+  :root[data-fr-theme='light'] {
+    .fr-input {
+      --border-action-high-blue-france: var(--blue-france-main-525);
+    }
+  }
+  @media (prefers-color-scheme: light) {
+    :root[data-fr-theme='system'] {
+      .fr-input {
+        --border-action-high-blue-france: var(--blue-france-main-525);
+      }
+    }
+  }
+</style>
