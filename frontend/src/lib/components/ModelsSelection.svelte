fd70545099a7085fc8d9c6c11d11cf33e814259b
axolotle <nchesnais@mailoo.org>
2025-08-26 16:08:23 +0200

    refactor(front): quick rework ModelsSelection

--- frontend/src/lib/components/ModelsSelection.svelte
+++ frontend/src/lib/components/ModelsSelection.svelte
@@ -2,7 +2,7 @@
   import { Badge } from '$lib/components/dsfr'
   import type { BotModel } from '$lib/models'
 
-  // FIXME rework as Selector
+  // FIXME ally, rework as Selector checkbox
   let {
     selection = $bindable([]),
     models,
@@ -23,7 +23,7 @@
   }
 </script>
 
-<div class="models-grid">
+<div class="grid grid-cols-2 gap-3 md:grid-cols-3">
   {#each models as { id, simple_name, icon_path, organisation, badges }, index}
     {@const modelBadges = (['license', 'releaseDate', 'size'] as const)
       .map((k) => badges[k])
@@ -36,7 +36,8 @@
       tabindex="0"
       role="radio"
       aria-checked={selection.includes(id) ? 'true' : 'false'}
-      on:keydown={(e) => handleKeyDown(id, e)}
+      class="p-2 md:px-4 md:py-3"
+      onkeydown={(e) => handleKeyDown(id, e)}
     >
       <input
         type="radio"
@@ -45,15 +46,16 @@
         data-index={index}
         aria-checked={selection.includes(id)}
         disabled={disabled || (!selection.includes(id) && selection.length == 2)}
-        on:click={(e) => {
+        onclick={(e) => {
           toggleModelSelection(id)
           e.stopPropagation()
         }}
       />
-      <div>
-        <span class="icon">
-          <img src="/orgs/ai/{icon_path}" alt={organisation} width="20" class="inline" />
-        </span><span class="organisation">{organisation}/</span><strong>{simple_name}</strong>
+      <div class="text-dark-grey flex">
+        <img src="/orgs/ai/{icon_path}" alt={organisation} class="me-2 w-[20px] object-contain" />
+        <span class="organisation hidden md:inline">{organisation}/</span><strong
+          >{simple_name}</strong
+        >
       </div>
       <ul class="fr-badges-group hidden! md:flex! mt-3!">
         {#each modelBadges as badge, i}
@@ -65,47 +67,27 @@
 </div>
 
 <style>
-  .models-grid {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 0.5em;
-  }
-
-  @media (min-width: 48em) {
-    .models-grid {
-      grid-template-columns: 1fr 1fr 1fr;
-      gap: 1em;
-    }
-  }
   label.selected,
   label.selected:active,
   label.selected:focus {
-    outline: 2px solid #6a6af4 !important;
+    outline: 2px solid var(--blue-france-main-525) !important;
   }
 
   /* label:not([disabled]):active { */
   label:active,
   label:focus {
-    outline: 2px solid #ccc !important;
+    outline: 2px solid var(--grey-950-125-active) !important;
   }
 
   label.disabled,
   label.disabled:active,
   label.disabled:focus {
     filter: grayscale(100%);
-    outline: 1px solid #ccc !important;
+    outline: 1px solid var(--grey-950-125-active) !important;
   }
   label {
     border-radius: 0.5em;
-    outline: 1px solid #e5e5e5;
-    display: grid;
-    align-items: center;
-    /* grid-template-columns: auto 1fr; */
-    padding: 0.25em 0;
-  }
-
-  label .icon {
-    padding: 0 0.5em 0 0.5em;
+    outline: 1px solid var(--grey-925-125);
   }
 
   input[type='radio'],
@@ -114,27 +96,9 @@
     opacity: 0 !important;
     pointer-events: none;
   }
-  /* p {
-		color: #666666 !important;
-		font-size: 0.875em !important;
-	} */
-  strong {
-    font-size: 0.875em;
-    color: #3a3a3a;
-  }
 
+  strong,
   .organisation {
-    color: rgb(36, 36, 36);
-    display: none;
     font-size: 0.875em;
-  }
-
-  @media (min-width: 48em) {
-    .organisation {
-      display: inline;
-    }
-    label {
-      padding: 1em;
-    }
   }
 </style>
