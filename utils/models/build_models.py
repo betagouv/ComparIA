92a5071776da019c05c3d2376ca339b0394f4d01
axolotle <nchesnais@mailoo.org>
2025-08-30 11:13:44 +0200

    feat(data): add deep sorting

--- utils/models/build_models.py
+++ utils/models/build_models.py
@@ -89,8 +89,9 @@ def filter_dict(data: Obj, exclude: list[str]) -> Obj:
     return {k: data[k] for k in set(data.keys()).difference(exclude)}
 
 
-def sort_dict(data: Obj) -> Obj:
-    return dict(sorted(data.items()))
+def sort_dict(data: Obj, deep: bool = True) -> Obj:
+    items = [(key, sort_dict(value) if isinstance(value, dict) else value ) for key, value in data.items()]
+    return dict(sorted(items, key=lambda i: i[0].lower()))
 
 
 def log_errors(errors: dict[str, list[Obj]]) -> None:
