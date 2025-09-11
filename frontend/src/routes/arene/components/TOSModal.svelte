2a5471d51a7dc0f910f710f992651d1d6fa81355
axolotle <nchesnais@mailoo.org>
2025-09-11 10:34:22 +0200

    refactor(front): rework TOSModal

--- frontend/src/routes/arene/components/TOSModal.svelte
+++ frontend/src/routes/arene/components/TOSModal.svelte
@@ -1,69 +1,89 @@
 <script lang="ts">
-  import { Button, Icon } from '$components/dsfr'
+  import { Button, Checkbox, Icon } from '$components/dsfr'
   import { useLocalStorage } from '$lib/helpers/useLocalStorage.svelte'
   import { m } from '$lib/i18n/messages'
+  import { propsToAttrs } from '$lib/utils/commons'
 
-  const visited = useLocalStorage('comparia:visited', false)
+  const acceptTos = useLocalStorage('comparia:tos', false)
+  let showModal = $state(!acceptTos.value)
+  let tosError = $state<string>()
+
+  $effect(() => {
+    if (acceptTos.value) tosError = undefined
+  })
+
+  function onClose(e: MouseEvent) {
+    if (!acceptTos.value) {
+      e.stopPropagation()
+      tosError = m['home.intro.tos.error']()
+    }
+  }
+
   const pratices = [
     { label: 'welcome.errors', icon: 'success-line' },
     { label: 'welcome.privacy', icon: 'pass-expired-line' },
     { label: 'welcome.use', icon: 'chat-delete-line' }
   ] as const
-
-  function onClose() {
-    visited.value = true
-  }
 </script>
 
-<button class="hidden" data-fr-opened={!visited.value} aria-controls="fr-modal-welcome">
-  Hidden
-</button>
+<button class="hidden" data-fr-opened={showModal} aria-controls="fr-modal-welcome"> Hidden </button>
 <dialog
   aria-labelledby="fr-modal-title-modal-welcome"
-  role="dialog"
   id="fr-modal-welcome"
   class="fr-modal"
   data-fr-concealing-backdrop="false"
 >
   <div class="fr-container fr-container--fluid fr-container-md">
     <div class="fr-grid-row fr-grid-row--center">
-      <div class="fr-col-12 fr-col-md-8 fr-col-lg-6">
+      <div class="fr-col-12 fr-col-md-8 fr-col-lg-9">
         <div class="fr-modal__body rounded-xl">
-          <div class="fr-modal__header">
-            <Button
-              variant="tertiary-no-outline"
-              text={m['words.close']()}
-              title={m['closeModal']()}
-              aria-controls="fr-modal-welcome"
-              class="fr-btn--close"
-            />
-          </div>
-          <div class="fr-modal__content">
-            <h1 id="fr-modal-title-modal-welcome" class="fr-modal__title">
-              {m['welcome.title']()}
-            </h1>
-
-            <p><strong>{m['welcome.goodPractices']()}</strong></p>
-
-            {#each pratices as { label, icon }}
-              <div class="mb-4 flex">
-                <Icon {icon} block class="text-primary me-2" />
-                <p class="mb-0!">{m[label]()}</p>
+          <div class="fr-modal__content mb-0! px-0!">
+            <div class="grid-cols-2 md:grid">
+              <div class="px-7 pb-7 pt-10">
+                <h2 id="fr-modal-title-modal-welcome" class="fr-modal__title mb-0! text-primary!">
+                  {m['welcome.title']()}
+                </h2>
               </div>
-            {/each}
-          </div>
-          <div class="fr-modal__footer">
-            <div
-              class="fr-btns-group fr-btns-group--right fr-btns-group--inline-reverse fr-btns-group--inline-lg fr-btns-group--icon-left"
-            >
-              <button
-                class="fr-btn purple-btn"
-                data-fr-opened="false"
-                aria-controls="fr-modal-welcome"
-                onclick={onClose}
-              >
-                {m['welcome.go']()}
-              </button>
+              <div class="bg-light-grey hidden md:block"></div>
+            </div>
+
+            <div class="grid-cols-2 md:grid">
+              <div class="px-7">
+                {#each pratices as { label, icon }}
+                  <div class="mb-7 md:last-of-type:mb-0">
+                    <Icon {icon} block size="lg" class="text-primary me-2" />
+                    <p class="mb-0! text-[14px]!">{m[label]()}</p>
+                  </div>
+                {/each}
+              </div>
+              <div class="bg-light-grey px-7 pt-7 md:pt-0">
+                <p class="mb-2!"><strong>{m['home.intro.tos.help']()}</strong></p>
+                <p class="mb-0! text-[14px]!">{m['welcome.tos.desc']()}</p>
+                <p class="text-[14px]!">
+                  <a href="/product/problem" target="_blank">{m['welcome.tos.moreInfos']()}</a>
+                </p>
+
+                <Checkbox
+                  bind:checked={acceptTos.value}
+                  id="tos-modal"
+                  label={m['home.intro.tos.accept']({
+                    linkProps: propsToAttrs({ href: '/modalites', target: '_blank' })
+                  })}
+                  error={tosError}
+                  class={{ 'mb-0!': !tosError }}
+                />
+              </div>
+            </div>
+
+            <div class="grid-cols-2 md:grid">
+              <div class="hidden md:block"></div>
+              <div class="bg-light-grey flex justify-end px-7 py-7">
+                <Button
+                  text={m['welcome.go']()}
+                  aria-controls="fr-modal-welcome"
+                  onclickcapture={(e) => onClose(e)}
+                />
+              </div>
             </div>
           </div>
         </div>
