51e1bb872d25fc40cb92ad6148f169c607eb01fe
axolotle <nchesnais@mailoo.org>
2025-09-15 11:39:38 +0200

    refactor(front): use generated types

--- frontend/src/lib/models.ts
+++ frontend/src/lib/models.ts
@@ -1,83 +1,9 @@
 import { getContext, setContext } from 'svelte'
+import { LICENSES, MODELS, ORGANISATIONS } from './generated'
 import { m } from './i18n/messages'
 
 export const SIZES = ['XS', 'S', 'M', 'L', 'XL'] as const
 export const ARCHS = ['moe', 'dense', 'matformer', 'maybe-moe', 'maybe-dense'] as const
-export const LICENSES = [
-  'proprietary',
-  'CC-BY-NC-4.0',
-  'Apache 2.0',
-  'Llama 3.1',
-  'Llama 4',
-  'Mistral AI Research License',
-  'Llama 3.3',
-  'MIT',
-  'Gemma'
-] as const
-export const ORGANISATIONS = [
-  'Meta',
-  'DeepSeek',
-  'Cohere',
-  'xAI',
-  'Microsoft',
-  'Google',
-  'Alibaba',
-  'Moonshot AI',
-  'Nvidia',
-  'OpenAI',
-  'Mistral AI',
-  'Amazon',
-  'Anthropic',
-  'Zhipu',
-  'Nous'
-] as const
-export const MODELS = [
-  'Llama 3.1 405B',
-  'Llama 3.3 70B',
-  'Llama 3.1 8B',
-  'Llama 4 Scout',
-  'Llama Maverick',
-  'DeepSeek R1',
-  'DeepSeek R1 Llama 70B',
-  'DeepSeek V3',
-  'Aya Expanse 32B',
-  'Command A',
-  'Command R',
-  'Grok 3 Mini',
-  'Phi-4',
-  'Gemini 2.5 Flash',
-  'Gemma 3n 4B',
-  'Gemma 3 4B',
-  'Gemma 3 12B',
-  'Gemma 3 27B',
-  'qwq 32B',
-  'Qwen 2.5 Coder 32B',
-  'Qwen 3 32B',
-  'Qwen 3 30B A3B',
-  'Qwen 2.5 max 0125',
-  'Kimi K2',
-  'Nemotron Llama 3.1 70B',
-  'GPT OSS-120B',
-  'GPT OSS-20B',
-  'GPT 5',
-  'GPT 5 Mini',
-  'GPT 5 Nano',
-  'GPT 4.1 Nano',
-  'GPT-4.1 Mini',
-  'o4 mini',
-  'Mistral Large 2',
-  'Mistral Saba',
-  'Mistral Small 3.2',
-  'Magistral Medium',
-  'Mistral Medium 2506',
-  'Mistral Medium 3.1',
-  'Magistral Small',
-  'Ministral',
-  'Claude 3.7 Sonnet',
-  'Claude 4 Sonnet',
-  'GLM 4.5',
-  'Hermes 3 405B'
-] as const
 
 export type Sizes = (typeof SIZES)[number]
 export type Archs = (typeof ARCHS)[number]
