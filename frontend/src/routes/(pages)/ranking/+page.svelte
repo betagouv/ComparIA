943a5cb86bd27dc9487215f30d1a26d685349eb4
axolotle <nchesnais@mailoo.org>
2025-09-18 13:06:02 +0200

    refactor(front): add Tabs to ranking page

--- frontend/src/routes/(pages)/ranking/+page.svelte
+++ frontend/src/routes/(pages)/ranking/+page.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
+  import { Tabs } from '$components/dsfr'
   import SeoHead from '$components/SEOHead.svelte'
   import { m } from '$lib/i18n/messages'
-  import { propsToAttrs, sanitize } from '$lib/utils/commons'
-  import RankingTable from './components/RankingTable.svelte'
+  import { sanitize } from '$lib/utils/commons'
+  import { Preferences, RankingTable } from './components'
+
+  const tabs = (
+    [
+      { id: 'ranking', icon: 'trophy-line' },
+      { id: 'energy', icon: 'bar-chart-2-line' },
+      { id: 'preferences', icon: 'bar-chart-2-line' },
+      { id: 'methodology' }
+    ] as const
+  ).map((tab) => ({
+    ...tab,
+    label: m[`ranking.${tab.id}.tabLabel`]()
+  }))
 </script>
 
 <SeoHead title={m['seo.titles.ranking']()} />
@@ -11,14 +24,18 @@
   <div class="fr-container">
     <h1 class="fr-h3 mb-10!">{m['ranking.title']()}</h1>
 
-    <p>
-      {@html sanitize(
-        m['ranking.desc']({
-          linkProps: propsToAttrs({ href: '#FIXME', class: 'text-primary!' })
-        })
-      )}
-    </p>
+    <Tabs {tabs} noBorders kind="nav" label={m['faq.title']()}>
+      {#snippet tab({ id })}
+        {#if id === 'ranking'}
+          <p class="text-[14px]! text-dark-grey mb-12!">
+            {@html sanitize(m['ranking.ranking.desc']())}
+          </p>
 
-    <RankingTable />
+          <RankingTable />
+        {:else if id === 'preferences'}
+          <Preferences />
+        {/if}
+      {/snippet}
+    </Tabs>
   </div>
 </main>
---
+++ frontend/src/routes/(pages)/ranking/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Preferences } from './Preferences.svelte'
+export { default as RankingTable } from './RankingTable.svelte'
