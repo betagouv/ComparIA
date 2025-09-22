25e455c2c9fb81d28d33140b834dfd96ed55353a
axolotle <nchesnais@mailoo.org>
2025-09-22 13:38:44 +0200

    feat(front): ranking: add graph tooltip

--- frontend/src/routes/(pages)/ranking/components/EnergyGraph.svelte
+++ frontend/src/routes/(pages)/ranking/components/EnergyGraph.svelte
@@ -8,17 +8,22 @@
 
   const modelsData = getModelsContext()
 
-  const points = $derived(
+  const models = $derived(
     modelsData
       .filter((m) => !!m.elo)
       .map((m) => ({
+        id: m.id,
         x: m.consumption_wh!,
         y: m.elo!,
-        size: m.license === 'proprietary' ? 'proprietary' : m.friendly_size
+        class: m.license === 'proprietary' ? 'proprietary' : m.friendly_size
       }))
   )
   // FIXME retrieve info from backend
   let lastUpdateDate = new Date()
+
+  let hoveredModel = $state<string>()
+  let tooltipPos = $state({ x: 0, y: 0 })
+  const hoveredModelData = $derived(modelsData.find((m) => m.id === hoveredModel))
 
   let svg = $state<SVGSVGElement>()
   let width = $state(1100)
@@ -27,11 +32,11 @@
   const padding = { top: 0, right: 0, bottom: 35, left: 40 }
 
   const minMaxX = $derived.by(() => {
-    const [min, max] = extent(points, (p) => p.x) as [number, number]
+    const [min, max] = extent(models, (m) => m.x) as [number, number]
     return [min - 5, max + 15] as const
   })
   const minMaxY = $derived.by(() => {
-    const [min, max] = extent(points, (p) => p.y) as [number, number]
+    const [min, max] = extent(models, (m) => m.y) as [number, number]
     return [min - 5, max + 35] as const
   })
   const xScale = $derived(scaleLinear(minMaxX, [padding.left, width - padding.right]))
@@ -44,6 +49,11 @@
   function resize() {
     ;({ width, height } = svg!.getBoundingClientRect())
   }
+
+  function onModelHover(model: (typeof models)[number]) {
+    hoveredModel = model.id
+    tooltipPos = { x: xScale(model.x), y: yScale(model.y) }
+  }
 </script>
 
 <svelte:window onresize={resize} />
@@ -53,7 +63,7 @@
     <Icon icon="thumb-up-line" class="text-primary" />
     <strong>{m['ranking.energy.views.graph.yLabel']()}</strong>
   </div>
-  <div class="flex-grow">
+  <div class="relative flex-grow">
     <svg id="energy-graph" bind:this={svg}>
       <!-- y axis -->
       <g class="axis y-axis">
@@ -76,10 +86,52 @@
       </g>
 
       <!-- data -->
-      {#each points as point}
-        <circle cx={xScale(point.x)} cy={yScale(point.y)} r="5" class={point.size} />
+      {#each models as m}
+        <circle
+          cx={xScale(m.x)}
+          cy={yScale(m.y)}
+          r="5"
+          class={m.class}
+          onpointerenter={() => onModelHover(m)}
+          onpointerleave={() => (hoveredModel = undefined)}
+        />
       {/each}
     </svg>
+
+    {#if hoveredModelData}
+      <div
+        id="graph-tooltip"
+        class="cg-border rounded-sm! absolute bg-white p-3"
+        style="--x: {tooltipPos.x}px; --y:{tooltipPos.y}px;"
+      >
+        <div class="flex">
+          <img
+            src="/orgs/ai/{hoveredModelData.icon_path}"
+            alt={hoveredModelData.organisation}
+            class="me-1 w-[14px] object-contain"
+          />
+          <strong class="text-[12px]">{hoveredModelData.id}</strong>
+        </div>
+
+        <div class="mt-1 text-[10px]">
+          <div class="flex gap-1 leading-relaxed">
+            <Icon icon="thumb-up-line" size="xxs" class="text-primary" />
+            <p class="mb-0! text-[10px]! text-grey leading-relaxed!">
+              {m['ranking.energy.views.graph.tooltip.elo']()}
+            </p>
+            <strong class="ms-auto">{hoveredModelData.elo}</strong>
+          </div>
+          <div class="flex gap-1 leading-relaxed">
+            <Icon icon="flashlight-line" size="xxs" class="text-primary" />
+            <p class="mb-0! text-[10px]! text-grey leading-relaxed!">
+              {m['ranking.energy.views.graph.tooltip.conso']()}
+            </p>
+            <strong class="ms-auto">{hoveredModelData.consumption_wh}</strong>
+          </div>
+        </div>
+      </div>
+    {/if}
+
     <div class="text-center">
       <Icon icon="flashlight-line" class="text-primary" />
       <strong>{m['ranking.energy.views.graph.xLabel']()}</strong>
@@ -149,5 +201,11 @@
         fill: var(--red-marianne-main-472);
       }
     }
+  }
+
+  #graph-tooltip {
+    top: var(--y);
+    left: var(--x);
+    transform: translate(-50%, calc(-100% - 0.75rem));
   }
 </style>
