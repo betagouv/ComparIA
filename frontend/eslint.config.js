d22e976851709dcd25a9022990d589ea39067b04
axolotle <nchesnais@mailoo.org>
2025-09-23 09:14:24 +0200

    chore(front): eslint rule no-explicit-any: warn

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -20,7 +20,10 @@ export default ts.config(
     languageOptions: {
       globals: { ...globals.browser, ...globals.node }
     },
-    rules: { 'no-undef': 'off' }
+    rules: {
+      'no-undef': 'off', // FIXME rm?
+      '@typescript-eslint/no-explicit-any': 'warn'
+    }
   },
   {
     files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
