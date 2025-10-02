196a0c56de9d9599f6fe561d6636d6dc92116498
Hadrien <ketsapiwiq@protonmail.com>
2025-10-02 15:51:25 +0200

    feat(front): add PUBLIC_DISABLED_LOCALES env flag

--- frontend/src/lib/global.svelte.ts
+++ frontend/src/lib/global.svelte.ts
@@ -1,12 +1,23 @@
 import { getContext, setContext } from 'svelte'
+import { env } from '$env/dynamic/public';
 
-export const LOCALES = [
-  // { code: 'da', short: 'DA', long: 'DA - Dansk'},
-  { code: 'fr', short: 'FR', long: 'FR - Français' },
-  { code: 'en', short: 'EN', long: 'EN - English' },
-  // { code: 'lt', short: 'LT', long: 'LT - Lietuvių' },
-  // { code: 'sv', short: 'SV', long: 'SV - Svensk' }
-] as const
+const disabledLocaleCodes = env.PUBLIC_DISABLED_LOCALES
+  ? env.PUBLIC_DISABLED_LOCALES.split(',').map(code => code.trim())
+  : null;
+
+type Locale = { code: string, short: string, long: string };
+
+const ALL_LOCALES = [
+  { code: 'da', short: 'DA', long: 'DA - Dansk'} as Locale,
+  { code: 'fr', short: 'FR', long: 'FR - Français' } as Locale,
+  { code: 'en', short: 'EN', long: 'EN - English' } as Locale,
+  { code: 'lt', short: 'LT', long: 'LT - Lietuvių' } as Locale,
+  { code: 'sv', short: 'SV', long: 'SV - Svensk' } as Locale
+] as const;
+
+export const LOCALES = ALL_LOCALES.filter((locale: Locale) => {
+  return !(disabledLocaleCodes?.includes(locale.code));
+});
 
 export type VotesData = { count: number; objective: number }
 
