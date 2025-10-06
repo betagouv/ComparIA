450e67d16ecd22ee22ecf2df7b5849af212fe20f
axolotle <nchesnais@mailoo.org>
2025-10-06 09:32:19 +0200

    feat(front): ranking: add target lines on point hover energy graph

--- frontend/src/routes/(pages)/ranking/components/EnergyGraph.svelte
+++ frontend/src/routes/(pages)/ranking/components/EnergyGraph.svelte
@@ -30,7 +30,7 @@
       .filter((m) => sizes.length === 0 || sizes.includes(m.friendly_size))
       .map((m) => {
         return {
-          id: m.id,
+          ...m,
           x: m.consumption_wh!,
           y: m.elo!,
           radius: dotSizes[m.friendly_size],
@@ -38,11 +38,10 @@
         }
       })
   )
-  const ELOMedian = data[Math.floor(data.length / 2)].elo!
 
   let hoveredModel = $state<string>()
   let tooltipPos = $state({ x: 0, y: 0 })
-  const hoveredModelData = $derived(data.find((m) => m.id === hoveredModel))
+  const hoveredModelData = $derived(models.find((m) => m.id === hoveredModel))
   const tooltipExtraData = $derived(
     hoveredModelData?.license === 'proprietary'
       ? (['arch'] as const)
@@ -53,7 +52,7 @@
   let width = $state(1100)
   let height = $state(570)
 
-  const padding = { top: 5, right: 10, bottom: 35, left: 40 }
+  const padding = { top: 5, right: 10, bottom: 35, left: 72 }
 
   const minMaxX = $derived.by(() => {
     const [min, max] = extent(models, (m) => m.x) as [number, number]
@@ -83,7 +82,9 @@
 <svelte:window onresize={resize} />
 
 <div id="energy-graph" class="flex items-center gap-2">
-  <div class="h-6 w-6 translate-y-[95px] -rotate-90 overflow-visible whitespace-nowrap text-center">
+  <div
+    class="-me-8 h-6 w-6 translate-y-[95px] -rotate-90 overflow-visible whitespace-nowrap text-center"
+  >
     <Icon icon="thumb-up-line" class="text-primary" />
     <strong>{m['ranking.energy.views.graph.yLabel']()}</strong>
   </div>
@@ -111,13 +112,33 @@
           {/each}
         </g>
 
+        <!-- target lines -->
+        {#if hoveredModelData}
+          <!-- y axis -->
+          <g class="target-line" transform="translate(0, {yScale(hoveredModelData.y)})">
+            <line x1={padding.left} x2={xScale(hoveredModelData.x)} />
+            <rect y="-15" x={padding.left - 72} width="78" height="30" rx="4" ry="4" />
+            <text x={padding.left - 32} y="+4">{hoveredModelData.y} ELO</text>
+          </g>
+
+          <!-- x axis -->
+          <g class="target-line" transform="translate({xScale(hoveredModelData.x)},0)">
+            <line y1={yScale(minMaxY[0])} y2={yScale(hoveredModelData.y)} />
+            <rect y={height - padding.bottom} x="-35" width="70" height="30" rx="4" ry="4" />
+            <text y={height - padding.bottom + 20}>{hoveredModelData.x} WH</text>
+          </g>
+        {/if}
+
         <!-- data -->
         {#each models as m}
           <circle
             cx={xScale(m.x)}
             cy={yScale(m.y)}
             r={m.radius}
-            class={m.class}
+            class={[
+              m.class,
+              { hovered: hoveredModel === m.id, blurred: hoveredModel && hoveredModel !== m.id }
+            ]}
             onpointerenter={() => onModelHover(m)}
             onpointerleave={() => (hoveredModel = undefined)}
           />
@@ -127,7 +148,7 @@
       {#if hoveredModelData}
         <div
           id="graph-tooltip"
-          class="cg-border rounded-sm! absolute min-w-[175px] bg-white p-3 drop-shadow-md"
+          class="cg-border rounded-sm! z-1 absolute min-w-[175px] bg-white p-3 drop-shadow-md"
           style="--x: {tooltipPos.x}px; --y:{tooltipPos.y}px;"
         >
           <div class="flex">
@@ -136,7 +157,7 @@
               alt={hoveredModelData.organisation}
               class="me-1 w-[14px] object-contain"
             />
-            <strong class="text-[12px]">{hoveredModelData.id}</strong>
+            <strong class="text-[12px] leading-normal">{hoveredModelData.id}</strong>
           </div>
 
           <div class="mt-1 text-[10px]">
@@ -146,7 +167,7 @@
                 <p class="mb-0! text-[10px]! text-grey leading-relaxed!">
                   {m[`ranking.energy.views.graph.tooltip.${item.key}`]()}
                 </p>
-                <strong class="ms-auto">{hoveredModelData.elo}</strong>
+                <strong class="ms-auto">{hoveredModelData[item.key]}</strong>
               </div>
             {/each}
 
@@ -251,9 +272,30 @@
       text-anchor: end;
     }
 
-    .elo-median {
-      stroke: var(--grey-625-425);
-      stroke-dasharray: 5;
+    .target-line {
+      line {
+        stroke: var(--color-grey);
+        stroke-dasharray: 5;
+        stroke-width: 2px;
+      }
+
+      text {
+        text-anchor: middle;
+        font-size: 14px;
+        fill: var(--color-white);
+        font-weight: 700;
+      }
+    }
+
+    circle {
+      &.hovered {
+        stroke: var(--color-dark-grey);
+        stroke-width: 2px;
+      }
+
+      &.blurred {
+        opacity: 0.5;
+      }
     }
 
     /* Dots color */
@@ -278,7 +320,12 @@
   #graph-tooltip {
     top: var(--y);
     left: var(--x);
-    transform: translate(-50%, calc(-100% - 0.75rem));
+
+    transform: translate(-50%, calc(-100% - 1.5rem));
+
+    @media (min-width: 36em) {
+      transform: translate(1.5rem, calc(-1.5rem));
+    }
   }
 
   #graph-legend {
