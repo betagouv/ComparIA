584dbb29aa8e2a152663184bf6188b86ac56755c
axolotle <nchesnais@mailoo.org>
2025-11-05 12:40:20 +0100

    feat(data): add build_peren_model_list script

---
+++ utils/models/build_peren_model_list.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, Field, RootModel
+
+from utils.models.build_models import (
+    CURRENT_FOLDER,
+    GENERATED_MODELS_PATH,
+    ROOT_PATH,
+    log,
+)
+from utils.models.utils import read_json, write_json
+
+PEREN_MODELS_DATA_PATH = (
+    CURRENT_FOLDER.parent / "ranking_methods" / "data" / "models_data.json"
+)
+
+
+class PerenModel(BaseModel):
+    name: str = Field(validation_alias="simple_name")
+    model_name: str = Field(validation_alias="id")
+    organization: str = Field(validation_alias="organisation")
+    license: str
+
+
+PerenModelList = RootModel[list[PerenModel]]
+
+
+def main() -> None:
+    # Generate model list based on generated models to avoid errors (data is supposed to be valid)
+    raw_models = [
+        model for model in read_json(GENERATED_MODELS_PATH)["models"].values()
+    ]
+    models = PerenModelList(raw_models).model_dump()
+
+    write_json(PEREN_MODELS_DATA_PATH, models, indent=2)
+
+    log.info(
+        f"Generation of '{PEREN_MODELS_DATA_PATH.relative_to(ROOT_PATH)}' is successfull!"
+    )
+
+
+if __name__ == "__main__":
+    main()
