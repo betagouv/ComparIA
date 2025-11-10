e93bb7d2b389cc838c12e3ec816d8926a04c9d5a
axolotle <nchesnais@mailoo.org>
2025-11-10 13:28:39 +0100

    fix(front): arena: send area z-index

--- frontend/src/routes/arene/components/ViewChat.svelte
+++ frontend/src/routes/arene/components/ViewChat.svelte
@@ -111,7 +111,7 @@
     <div
       bind:this={footer}
       id="send-area"
-      class="z-1 bg-very-light-grey sticky bottom-0 mt-auto flex flex-col items-center gap-3 px-4 py-3 md:px-[20%]"
+      class="z-2 bg-very-light-grey sticky bottom-0 mt-auto flex flex-col items-center gap-3 px-4 py-3 md:px-[20%]"
     >
       {#if step === 'chat'}
         <div class="flex w-full flex-col gap-3 md:flex-row">
