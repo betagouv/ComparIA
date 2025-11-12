2d21de59eb4ed20faf5a8db6549e9881b930b452
axolotle <nchesnais@mailoo.org>
2025-11-12 16:15:21 +0100

    feat(front): add news empty page

---
+++ frontend/src/routes/(pages)/news/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import SeoHead from '$components/SEOHead.svelte'
+  import { m } from '$lib/i18n/messages'
+</script>
+
+<SeoHead title={m['seo.titles.news']()} />
+
+<main>
+  <div class="fr-container py-12">
+    <h2 class="mb-7!">Actualités - France</h2>
+  </div>
+</main>
