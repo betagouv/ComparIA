406e7749b6347189b6ce4b98f7b747952a9b03bb
axolotle <nchesnais@mailoo.org>
2025-11-12 12:25:34 +0100

    chore(front): lint

--- frontend/src/lib/global.svelte.ts
+++ frontend/src/lib/global.svelte.ts
@@ -1,23 +1,23 @@
+import { env } from '$env/dynamic/public'
 import { getContext, setContext } from 'svelte'
-import { env } from '$env/dynamic/public';
 
 const disabledLocaleCodes = env.PUBLIC_DISABLED_LOCALES
-  ? env.PUBLIC_DISABLED_LOCALES.split(',').map(code => code.trim())
-  : null;
+  ? env.PUBLIC_DISABLED_LOCALES.split(',').map((code) => code.trim())
+  : null
 
-type Locale = { code: string, short: string, long: string };
+type Locale = { code: string; short: string; long: string }
 
 const ALL_LOCALES = [
-  { code: 'da', short: 'DA', long: 'DA - Dansk'} as Locale,
+  { code: 'da', short: 'DA', long: 'DA - Dansk' } as Locale,
   { code: 'fr', short: 'FR', long: 'FR - Français' } as Locale,
   { code: 'en', short: 'EN', long: 'EN - English' } as Locale,
   { code: 'lt', short: 'LT', long: 'LT - Lietuvių' } as Locale,
   { code: 'sv', short: 'SV', long: 'SV - Svensk' } as Locale
-] as const;
+] as const
 
 export const LOCALES = ALL_LOCALES.filter((locale: Locale) => {
-  return !(disabledLocaleCodes?.includes(locale.code));
-});
+  return !disabledLocaleCodes?.includes(locale.code)
+})
 
 export type VotesData = { count: number; objective: number }
 
