d2e3e8c3628685824570ece346fac04aefa81c06
axolotle <nchesnais@mailoo.org>
2025-11-13 16:52:28 +0100

    feat(front): add custom locale strategy

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,23 +1,28 @@
+import { env } from '$env/dynamic/private'
+import { defineCustomServerStrategy } from '$lib/i18n/runtime'
 import { paraglideMiddleware } from '$lib/i18n/server'
 import type { Handle } from '@sveltejs/kit'
-import { env } from '$env/dynamic/private'
 
 const MATOMO_ID = env.MATOMO_ID || ''
 const MATOMO_URL = env.MATOMO_URL || ''
 
+defineCustomServerStrategy('custom-url', {
+  getLocale: (request) => {
+    if (!request) return
+
+    return {
+      'ai-arenaen.dk': 'da'
+    }[new URL(request.url).hostname]
+  }
+})
+
 // creating a handle to use the paraglide middleware
 const paraglideHandle: Handle = ({ event, resolve }) => {
-  const localeFromCookie = event.cookies.get('PARAGLIDE_LOCALE')
-  const localeFromHost = {
-    'ai-arenaen.dk': 'da'
-  }[event.url.hostname]
-
-  if (localeFromHost && localeFromHost !== localeFromCookie) {
-    event.cookies.set('PARAGLIDE_LOCALE', localeFromHost, { path: '/', httpOnly: false })
-  }
-
   return paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
     event.request = localizedRequest
+    if (locale !== event.cookies.get('PARAGLIDE_LOCALE')) {
+      event.cookies.set('PARAGLIDE_LOCALE', locale, { path: '/', httpOnly: false })
+    }
 
     return resolve(event, {
       transformPageChunk: ({ html }) => {
