df348dd5bf1fe794fea63b84f6244ac94349869b
axolotle <nchesnais@mailoo.org>
2025-12-09 13:33:27 +0100

    refactor(back): logger: use settings + move needed utils

--- languia/utils.py
+++ languia/utils.py
@@ -91,39 +91,6 @@ class EmptyResponseError(RuntimeError):
         return msg
 
 
-def get_ip(request: Request):
-    """
-    Extract user's real IP address from Gradio request headers.
-
-    Handles proxy chains and cloud protection services by checking multiple headers.
-    Priority order: cloud-protector-client-ip > x-original-forwarded-for > x-forwarded-for > request.client.host
-
-    Args:
-        request: Gradio Request object with headers
-
-    Returns:
-        str: User's IP address (first IP if multiple comma-separated values)
-    """
-    # Try cloud protection provider IP first (OVH, etc.)
-    if "cloud-protector-client-ip" in request.headers:
-        ip = request.headers["cloud-protector-client-ip"]
-    # Try original forwarded IP (before multiple proxies)
-    elif "x-original-forwarded-for" in request.headers:
-        ip = request.headers["x-original-forwarded-for"]
-    # Try standard forwarded-for header
-    elif "x-forwarded-for" in request.headers:
-        ip = request.headers["x-forwarded-for"]
-    # Fall back to direct client IP
-    else:
-        ip = request.client.host
-
-    # Multiple IPs can be returned as comma-separated string; take the first (client IP)
-    if "," in ip:
-        ip = ip.split(",")[0].strip()
-
-    return ip
-
-
 def get_chosen_model(which_model_radio):
     """
     Extract model choice from radio button value.
