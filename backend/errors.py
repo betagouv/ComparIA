9f6207c89f6318ed85fecd0902a4021c00d1454a
axolotle <nchesnais@mailoo.org>
2026-01-13 16:23:35 +0100

    refactor(back): i18n errors as TODO

--- backend/errors.py
+++ backend/errors.py
@@ -1,5 +1,5 @@
 from enum import StrEnum
 
-
-class Errors(StrEnum):
-    RATE_LIMITED = "rate_limited"  # "Vous avez trop sollicité les modèles parmi les plus onéreux, veuillez réessayer dans quelques heures. Vous pouvez toujours solliciter des modèles plus petits."
+# TODO raise errors with error keys and add i18n on front
+# class Errors(StrEnum):
+#     RATE_LIMITED = "rate_limited"  # "Vous avez trop sollicité les modèles parmi les plus onéreux, veuillez réessayer dans quelques heures. Vous pouvez toujours solliciter des modèles plus petits."
