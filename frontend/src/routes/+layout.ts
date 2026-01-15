0a46e42676a3f19922dbae97d590422b0d00f1c7
axolotle <nchesnais@mailoo.org>
2026-01-15 20:32:18 +0100

    fix(front): api call inn layout.ts

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,4 +1,4 @@
-import { api } from '$lib/api'
+import { api } from '$lib/fastapi-client'
 import type { VotesData } from '$lib/global.svelte'
 import { getLocale } from '$lib/i18n/runtime'
 import type { APIData } from '$lib/models'
@@ -6,9 +6,11 @@ import type { APIData } from '$lib/models'
 export async function load() {
   // Query votes depending on locale
   // Will return specific counter if locale is a country_portal else the default
-  const votes = await api.get<VotesData>(`/counter?country_portal=${getLocale()}`)
+  const votes = await api.request<VotesData>(`/counter?country_portal=${getLocale()}`, {
+    method: 'GET'
+  })
 
-  const data = await api.get<APIData>('/models')
+  const data = await api.request<APIData>('/models', { method: 'GET' })
 
   return { data, votes }
 }
