ce797139d9944e352eeaad80bc79a0e99126d7da
axolotle <nchesnais@mailoo.org>
2026-01-21 15:29:35 +0100

    feat(front): add global error catcher

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,13 +4,13 @@
   import { page } from '$app/state'
   import Toaster from '$components/Toaster.svelte'
   import { setI18nContext, setVotesContext } from '$lib/global.svelte'
+  import { useToast } from '$lib/helpers/useToast.svelte'
   import { setModelsContext } from '$lib/models'
   import { setCohortContext } from '$lib/stores/cohortStore.svelte'
   import { onMount } from 'svelte'
   import { SvelteURLSearchParams } from 'svelte/reactivity'
   import 'uno.css'
   import '../css/app.css'
-
   if (browser) {
     // FIXME import only needed parts?
     // @ts-expect-error - DSFR module import
@@ -30,7 +30,14 @@
   setModelsContext(data.data)
   setI18nContext()
   setCohortContext()
+
+  function handleError(_event: PromiseRejectionEvent) {
+    // FIXME display error page on some error? display custom text in toast?
+    useToast('Unexpected error', 10000, 'error')
+  }
 </script>
+
+<svelte:window on:unhandledrejection={handleError} />
 
 <Toaster />
 
