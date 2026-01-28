c9937f16163a844dfd70aea69b0b3263dda5f598
axolotle <nchesnais@mailoo.org>
2026-01-28 20:44:50 +0100

    refactor(front): remove country_portal query param from /counter call (passed as header now)

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,12 +1,11 @@
 import { api } from '$lib/fastapi-client'
 import type { VotesData } from '$lib/global.svelte'
-import { getLocale } from '$lib/i18n/runtime'
 import type { APIData } from '$lib/models'
 
 export async function load() {
-  // Query votes depending on locale
+  // Query votes depending on locale (sent as header)
   // Will return specific counter if locale is a country_portal else the default
-  const votes = await api.request<VotesData>(`/counter?country_portal=${getLocale()}`, {
+  const votes = await api.request<VotesData>(`/counter`, {
     method: 'GET'
   })
 
