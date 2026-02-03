d5cf4fc828747e8b275ee3bc99b2d18b69aa2a18
Elie Gavoty <eliegavoty@free.fr>
2026-02-03 19:06:07 +0100

    controller(dashboard): fix pydantic serialization issue

--- controller.py
+++ controller.py
@@ -8,7 +8,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 
-from backend.llms.data import get_models
+from backend.config import DEFAULT_COUNTRY_PORTAL
+from backend.llms.data import get_llms_data
 
 templates = Jinja2Templates(directory="templates")
 
@@ -55,7 +56,7 @@ def get_models_errors() -> list[str]:  # Return type hint
     response_class=HTMLResponse,
 )
 def index(request: Request) -> HTMLResponse:
-    models = get_models()
+    models = get_llms_data(DEFAULT_COUNTRY_PORTAL)
 
     error_count = dict.fromkeys(models.enabled, 0)
     for model_id, _date, _details in models_errors:
@@ -67,7 +68,7 @@ def index(request: Request) -> HTMLResponse:
         {
             "models_errors": models_errors,
             "error_count": error_count,
-            "models": models.enabled,
+            "models": {k: v.model_dump() for k, v in models.enabled.items()},
             "big_models": models.big_models,
             "small_models": models.small_models,
             "random_pool": models.random_models,
