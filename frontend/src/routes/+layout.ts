9cd2d8638cf28ee0811e870b6dc75dc66df683b1
Elie Gavoty <eliegavoty@free.fr>
2026-02-16 15:29:15 +0100

    fix(front): disable counterLocale mechanism

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,13 +1,13 @@
 import { api } from '$lib/fastapi-client'
-import type { VotesData } from '$lib/global.svelte'
 import type { APIData } from '$lib/models'
 
 export async function load() {
   // Query votes depending on locale (sent as header)
   // Will return specific counter if locale is a country_portal else the default
   // Query votes depending on locale - only da or fr are valid for counter
-  const counterLocale = locale === 'da' ? 'da' : 'fr'
-  const votes = await api.get<VotesData>(`/counter?c=${counterLocale}`)
+  // const counterLocale = locale === 'da' ? 'da' : 'fr'
+  // const votes = await api.get<VotesData>(`/counter?c=${counterLocale}`)
+  const votes = await api.get<VotesData>(`/counter`)
   const data = await api.request<APIData>('/models/', { method: 'GET' })
 
   return { data, votes }
