d6fabe802836175d922aaa7766e8885eb4958110
Elie Gavoty <eliegavoty@free.fr>
2026-04-01 18:17:45 +0200

    devops: drastic simplification following jenkins pipeline cleaning : only use deploy-dev etc

--- docker/jenkins/jenkins_common/__init__.py
+++ docker/jenkins/jenkins_common/__init__.py
@@ -3,13 +3,9 @@ Module commun pour les triggers Jenkins
 """
 
 from .base import JenkinsJobTrigger
-from .build_job import JenkinsBuildJobTrigger
 from .deploy_job import JenkinsDeployJobTrigger
-from .promotion_job import JenkinsPromotionJobTrigger
 
 __all__ = [
     "JenkinsJobTrigger",
-    "JenkinsBuildJobTrigger",
     "JenkinsDeployJobTrigger",
-    "JenkinsPromotionJobTrigger",
 ]
