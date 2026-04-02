d104a5d02c4350ee67a2e7c5ef15918b0c6d1981
axolotle <nchesnais@mailoo.org>
2026-04-02 18:27:41 +0200

    feat(database): add lint db utility

---
+++ utils/database/lint.py
@@ -0,0 +1,28 @@
+import logging
+
+from .actions import (
+    archive_corrupted,
+    archive_not_archived,
+    archive_spam,
+    archive_unknown_llms,
+)
+from .utils import reset_archived
+
+logger = logging.getLogger("comparia.database")
+
+
+def lint(*, fix: bool = False, hard: bool = False):
+    """
+    Run database linting.
+
+    Will check for corrupted data, spam, unknown LLMs and not archived votes or reaction that should be.
+    Will only log what should be archived, use `--fix` to actually archive data.
+    Will only check not already analyzed data, use `--hard` to analyze/fix all data except already archived data.
+    """
+    if hard:
+        reset_archived()
+
+    archive_corrupted(commit=fix)
+    archive_spam(commit=fix)
+    archive_unknown_llms(commit=fix and hard)
+    archive_not_archived(commit=fix)
