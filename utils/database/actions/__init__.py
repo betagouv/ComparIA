b0201ece2f3e2a088cdf79d3fe0ca7d09ba8326f
axolotle <nchesnais@mailoo.org>
2026-04-02 18:22:48 +0200

    feat(database): add __init__

---
+++ utils/database/actions/__init__.py
@@ -0,0 +1,5 @@
+from .archive_corrupted import archive_corrupted
+from .archive_not_archived import archive_not_archived
+from .archive_spam import archive_spam
+from .archive_unknown_llms import archive_unknown_llms
+from .rename_llm import rename_llm
