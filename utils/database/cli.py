1b5c012aafd64a30c982d2f8ea921947adde73f7
axolotle <nchesnais@mailoo.org>
2026-04-02 18:29:50 +0200

    feat(database): add database cli tool

---
+++ utils/database/cli.py
@@ -0,0 +1,30 @@
+import logging
+
+from cyclopts import App
+
+from utils.logger import configure_logger
+
+from .actions import (
+    archive_corrupted,
+    archive_not_archived,
+    archive_spam,
+    archive_unknown_llms,
+    rename_llm,
+)
+from .lint import lint
+
+cli_archive = App(name="archive", help="Individual archival utilities.")
+cli_archive.command(archive_corrupted, name="corrupted")
+cli_archive.command(archive_not_archived, name="not_archive")
+cli_archive.command(archive_spam, name="spam")
+cli_archive.command(archive_unknown_llms, name="unknown_llms")
+
+cli_db = App(name="db", help="Database related utilities.")
+cli_db.command(lint)
+cli_db.command(rename_llm)
+cli_db.command(cli_archive)
+
+
+if __name__ == "__main__":
+    configure_logger(logging.getLogger("comparia.database"))
+    cli_db()
