1898559e75295bb280937d5cfd5a5783591b4a39
axolotle <nchesnais@mailoo.org>
2026-04-08 14:28:52 +0200

    feat(database): rework corrupted data archival loop

--- utils/database/actions/archive_corrupted.py
+++ utils/database/actions/archive_corrupted.py
@@ -5,16 +5,13 @@ from sqlalchemy import text
 
 from utils.utils import db_connection
 
-from ..utils import TABLE_NAMES, archive
+from ..utils import TABLE_NAMES, ArchivedReason, TableName, archive
 
 logger = logging.getLogger("comparia.database")
 
 # TODO those could be repared
-CORRUPTED_CONVERSATIONS_QUERY = """
-    SELECT 
-        conversation_pair_id
-    FROM 
-        conversations
+CORRUPTED_MODEL_STREAM_CONVERSATIONS_QUERY = """
+    SELECT conversation_pair_id FROM conversations
     WHERE
         archived IS NULL
         AND (
@@ -25,16 +22,7 @@ CORRUPTED_CONVERSATIONS_QUERY = """
 """
 
 CORRUPTED_REACTIONS_QUERY = """
-    SELECT 
-        id,
-        conversation_pair_id,
-        conversation_a,
-        conversation_b,
-        msg_index,
-        msg_rank,
-        chatbot_index
-    FROM 
-        reactions
+    SELECT id FROM reactions
     WHERE
         archived IS NULL
         -- Filter potentially incoherent data
@@ -68,31 +56,37 @@ def archive_corrupted(*, commit: bool = False) -> None:
     """
     Archive conversations, votes and reaction with corrupted data.
     """
-    logger.info("Searching for corrupted conversations")
-
     archived_at = datetime.now()
-    with db_connection(stream=True) as conn:
-        conv_results = conn.execute(text(CORRUPTED_CONVERSATIONS_QUERY)).all()
-        ids = [result[0] for result in conv_results]
+    logger.info("Searching for corrupted data")
+    queries: dict[TableName, dict[ArchivedReason, str]] = {
+        "conversations": {
+            "corrupted_model_stream": CORRUPTED_MODEL_STREAM_CONVERSATIONS_QUERY,
+        },
+        "reactions": {
+            "corrupted": CORRUPTED_REACTIONS_QUERY,
+        },
+    }
 
-    if not ids:
-        logger.info("No corrupted conversations found!")
-    else:
-        logger.warning(f"Found {len(ids)} 'conversations' with corrupted content.")
+    for table_name, qs in queries.items():
+        logger.info(f"Searching for corrupted {table_name}")
 
-        for table_name in TABLE_NAMES:
-            # archive corrupted 'conversations' and related 'votes' + 'reactions'
-            archive(table_name, ids, "corrupted", archived_at, commit=commit)
+        for reason, query in qs.items():
+            with db_connection(stream=True) as conn:
+                conv_results = conn.execute(text(query)).all()
+                ids = [result[0] for result in conv_results]
 
-    logger.info("Searching for corrupted reactions")
+            if not ids:
+                logger.info(f"No '{reason}' {table_name} found!")
+            else:
+                logger.warning(
+                    f"Found {len(ids)} {table_name} with corrupted content: '{reason}'."
+                )
 
-    with db_connection(stream=True) as conn:
-        reactions_results = conn.execute(text(CORRUPTED_REACTIONS_QUERY)).all()
-        ids = [result[0] for result in reactions_results]
-
-    if not ids:
-        logger.info("No corrupted reactions found!")
-    else:
-        logger.warning(f"Found {len(ids)} 'reactions' with corrupted content.")
-
-        archive("reactions", ids, "corrupted", archived_at, id_key="id", commit=commit)
+                if table_name == "conversations":
+                    for tb in TABLE_NAMES:
+                        # archive corrupted 'conversations' and related 'votes' + 'reactions'
+                        archive(tb, ids, reason, archived_at, commit=commit)
+                else:
+                    archive(
+                        table_name, ids, reason, archived_at, id_key="id", commit=commit
+                    )
