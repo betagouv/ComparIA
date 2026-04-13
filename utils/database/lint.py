9aade547066a6b8a635b3b967f30983e70206346
axolotle <nchesnais@mailoo.org>
2026-04-13 17:44:07 +0200

    refactor(database): improve table archived display + add 'order_by' arg + add 'days' arg to also display infos within last n days

--- utils/database/lint.py
+++ utils/database/lint.py
@@ -1,7 +1,9 @@
 import logging
 from collections import defaultdict
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import Literal, TypedDict
 
+import numpy as np
 import polars as pl
 
 from utils.utils import db_connection
@@ -25,59 +27,79 @@ from .utils import (
 logger = logging.getLogger("comparia.database")
 
 
-def log_archived() -> None:
+def log_archived(
+    *,
+    order_by: Literal[
+        "archived_reason",
+        "count",
+        "total",
+        "percent",
+        "last_ts",
+        "last_n_days_count",
+        "last_n_days_total",
+        "last_n_days_count_percent",
+    ] = "archived_reason",
+    descending: bool = True,
+    days: int = 0,
+) -> None:
     """
     Log archived data infos.
     """
     query = """
-        SELECT 
+        SELECT
+            archived,
             archived_reason, 
             timestamp 
         FROM 
             {table_name} 
-        WHERE archived = TRUE;
+        ;
     """
-    archived_counts: dict[ArchivedReason, dict[TableName, int]] = defaultdict(
-        lambda: defaultdict(int)
-    )
+    last_n_date = datetime.now() - timedelta(days=days)
+    to_fixed_float = lambda x: np.trunc(x * 100) / 100
 
-    archived_last_time_seen: dict[ArchivedReason, dict[TableName, datetime]] = (
-        defaultdict(lambda: defaultdict())
-    )
-    total_archived_counts: dict[TableName, int] = defaultdict(int)
     with db_connection(stream=True) as conn:
         for table_name in TABLE_NAMES:
             results = pl.read_database(
                 query=query.format(table_name=table_name),
                 connection=conn,
             )
-            total_archived_counts[table_name] = len(results)
-            for group in (
-                results.group_by("archived_reason")
-                .agg(
-                    count=pl.col("archived_reason").len(),
-                    last_time_seen=pl.col("timestamp").sort().last(),
-                )
-                .to_dicts()
-            ):
-                archived_counts[group["archived_reason"]][table_name] = group["count"]
-                archived_last_time_seen[group["archived_reason"]][table_name] = group[
-                    "last_time_seen"
-                ]
+            last_items_count = len(results.filter(pl.col("timestamp") > last_n_date))
+            archived_results = results.filter(pl.col("archived"))
 
-    total_counts = ", ".join(
-        f"{total_archived_counts[table_name]} {table_name}"
-        for table_name in TABLE_NAMES
-    )
-    logger.info(f"Total archived: {total_counts}")
-    reasons = "With reasons:"
-    for reason, tables in archived_counts.items():
-        counts = ", \n".join(
-            f"    {count} {table_name} (last timestamp: {archived_last_time_seen[reason][table_name].date()})"
-            for table_name, count in tables.items()
-        )
-        reasons += f"\n'{reason}':\n{counts}"
-    logger.info(reasons)
+            last_n_days_count = (
+                pl.col("timestamp").filter(pl.col("timestamp") > last_n_date).len()
+            )
+            reason_count = pl.col("archived_reason").len()
+
+            archived_reasons = (
+                archived_results.group_by("archived_reason")
+                .agg(
+                    count=reason_count,
+                    total=len(results),
+                    percent=to_fixed_float(reason_count / len(results) * 100),
+                    last_ts=pl.col("timestamp").sort().last().dt.date(),
+                    last_n_days_count=last_n_days_count,
+                    last_n_days_total=last_items_count,
+                    last_n_days_count_percent=to_fixed_float(
+                        last_n_days_count / last_items_count * 100
+                    ),
+                )
+                .sort(pl.col(order_by), descending=descending)
+            )
+
+            with pl.Config(tbl_cols=-1, tbl_rows=-1, set_tbl_hide_dataframe_shape=True):
+                total = archived_reasons.sum()
+                total[0, "archived_reason"] = "TOTAL"
+                total[0, "total"] = len(results)
+                total[0, "last_n_days_total"] = last_items_count
+                final = pl.concat([archived_reasons, total])
+                if not days:
+                    final = final.drop(
+                        "last_n_days_count",
+                        "last_n_days_total",
+                        "last_n_days_count_percent",
+                    )
+                logger.info(f"\nArchived '{table_name}' infos:\n{final}")
 
 
 def lint(*, fix: bool = False, hard: bool = False):
@@ -93,7 +115,7 @@ def lint(*, fix: bool = False, hard: bool = False):
     if hard:
         reset_archived()
 
-    # archive_spam(commit=fix) FIXME rm? done in topic_pii.py
+    archive_spam(commit=fix)
     archive_or_fix_country_portal(commit=fix)
     archive_corrupted(commit=fix)
     archive_unknown_llms(commit=fix and hard)
