24e841947a7f48c876f6f7dcfb3f1a8970db13a4
axolotle <nchesnais@mailoo.org>
2026-04-14 12:47:32 +0200

    refactor(database): log_archived allow to display only last n days data or display alongside global data

--- utils/database/lint.py
+++ utils/database/lint.py
@@ -1,6 +1,5 @@
 import logging
-from collections import defaultdict
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from typing import Literal, TypedDict
 
 import numpy as np
@@ -38,9 +37,10 @@ def log_archived(
         "last_n_days_count",
         "last_n_days_total",
         "last_n_days_count_percent",
-    ] = "archived_reason",
+    ] = "count",
     descending: bool = True,
     days: int = 0,
+    compare: bool = False,
 ) -> None:
     """
     Log archived data infos.
@@ -54,52 +54,70 @@ def log_archived(
             {table_name} 
         ;
     """
-    last_n_date = datetime.now() - timedelta(days=days)
+    last_n_date = datetime.combine(date.today(), datetime.min.time()) - timedelta(
+        days=days
+    )
+    last_n_only = not compare and days
+
     to_fixed_float = lambda x: np.trunc(x * 100) / 100
 
     with db_connection(stream=True) as conn:
         for table_name in TABLE_NAMES:
-            results = pl.read_database(
+            all_items = pl.read_database(
                 query=query.format(table_name=table_name),
                 connection=conn,
             )
-            last_items_count = len(results.filter(pl.col("timestamp") > last_n_date))
-            archived_results = results.filter(pl.col("archived"))
+            all_items_count = len(all_items)
+            last_items_count = len(all_items.filter(pl.col("timestamp") > last_n_date))
 
             last_n_days_count = (
                 pl.col("timestamp").filter(pl.col("timestamp") > last_n_date).len()
             )
             reason_count = pl.col("archived_reason").len()
+            cols = {f"last_n_days_{col}": col for col in ["count", "total", "percent"]}
 
             archived_reasons = (
-                archived_results.group_by("archived_reason")
+                all_items.filter(pl.col("archived"))
+                .group_by("archived_reason")
                 .agg(
-                    count=reason_count,
-                    total=len(results),
-                    percent=to_fixed_float(reason_count / len(results) * 100),
                     last_ts=pl.col("timestamp").sort().last().dt.date(),
+                    count=reason_count,
+                    total=all_items_count,
+                    percent=to_fixed_float(reason_count / all_items_count * 100),
                     last_n_days_count=last_n_days_count,
                     last_n_days_total=last_items_count,
-                    last_n_days_count_percent=to_fixed_float(
+                    last_n_days_percent=to_fixed_float(
                         last_n_days_count / last_items_count * 100
                     ),
                 )
+                .drop(
+                    *(
+                        cols.values()
+                        if last_n_only
+                        else (cols.keys() if not days else [])
+                    )
+                )
+                .rename(cols if last_n_only else {})
                 .sort(pl.col(order_by), descending=descending)
+                .filter(pl.col("count") != 0)
+            )
+
+            total = archived_reasons.sum()
+            total[0, "archived_reason"] = "TOTAL"
+            total[0, "total"] = last_items_count if last_n_only else all_items_count
+            if compare and days:
+                total[0, "last_n_days_total"] = last_items_count
+
+            final = (
+                pl.concat([archived_reasons, total])
+                .fill_nan(pl.lit(None))
+                .fill_null(pl.lit(""))
             )
 
             with pl.Config(tbl_cols=-1, tbl_rows=-1, set_tbl_hide_dataframe_shape=True):
-                total = archived_reasons.sum()
-                total[0, "archived_reason"] = "TOTAL"
-                total[0, "total"] = len(results)
-                total[0, "last_n_days_total"] = last_items_count
-                final = pl.concat([archived_reasons, total])
-                if not days:
-                    final = final.drop(
-                        "last_n_days_count",
-                        "last_n_days_total",
-                        "last_n_days_count_percent",
-                    )
-                logger.info(f"\nArchived '{table_name}' infos:\n{final}")
+                logger.info(
+                    f"\nArchived '{table_name}' infos{f' since {last_n_date}' if days else ''}:\n{final}"
+                )
 
 
 def lint(*, fix: bool = False, hard: bool = False):
