baa775f828b2335bbfe3eef0b3ee7f8a41093259
axolotle <nchesnais@mailoo.org>
2026-04-20 10:21:54 +0200

    feat(db): add Turn model

---
+++ utils/database/models/turn.py
@@ -0,0 +1,40 @@
+from typing import Annotated
+
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlmodel import Field, Relationship, SQLModel, String
+
+from backend.config import NegativePref, PositivePref, TurnChoice
+
+from .messages import LLMMessage, UserMessage
+
+LLMMessageId = Annotated[int | None, Field(foreign_key="llm_message.id", unique=True)]
+KeywordAnnotations = Annotated[
+    list[PositivePref] | list[NegativePref], Field(sa_type=JSONB)
+]
+
+
+class Turn(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    choice: Annotated[TurnChoice | None, Field(sa_type=String)]
+    user_msg: UserMessage = Relationship(
+        sa_relationship_kwargs={"uselist": False, "lazy": "joined"}
+    )
+
+    # a
+    llm_id_a: str
+    llm_msg_a_id: LLMMessageId = None
+    llm_msg_a: LLMMessage | None = Relationship(
+        sa_relationship_kwargs={"foreign_keys": "[Turn.llm_msg_a_id]", "lazy": "joined"}
+    )
+    keyword_annotations_a: KeywordAnnotations = []
+    custom_annotation_a: str | None = None
+
+    # b
+    llm_id_b: str
+    llm_msg_b_id: LLMMessageId = None
+    llm_msg_b: LLMMessage | None = Relationship(
+        sa_relationship_kwargs={"foreign_keys": "[Turn.llm_msg_b_id]", "lazy": "joined"}
+    )
+    keyword_annotations_b: KeywordAnnotations = []
+    custom_annotation_b: str | None = None
