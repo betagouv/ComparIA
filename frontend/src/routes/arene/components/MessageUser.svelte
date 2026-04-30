958b58b65d78a649b9106374af7bb74bd881316b
axolotle <nchesnais@mailoo.org>
2026-04-30 16:22:10 +0200

    refactor(front): display mode infos on first user message

--- frontend/src/routes/arene/components/MessageUser.svelte
+++ frontend/src/routes/arene/components/MessageUser.svelte
@@ -1,31 +1,16 @@
 <script lang="ts">
   import Markdown from '$components/markdown/MarkdownCode.svelte'
   import type { UserMessage } from '$lib/chatService.svelte'
-  import { onMount } from 'svelte'
 
   export type MessageUserProps = {
     message: UserMessage
-    size: number
   }
 
-  let { message, size = $bindable() }: MessageUserProps = $props()
-
-  let elem = $state<HTMLDivElement>()
-
-  onMount(() => {
-    size = elem!.offsetHeight
-  })
+  let { message }: MessageUserProps = $props()
 </script>
 
-<div
-  bind:this={elem}
-  class="message-user md:max-w-3/5 mb-4 mt-5 rounded-2xl px-5 py-3 md:mb-8 md:ms-auto bg-[--grey-950-100]"
->
+<div class="message-user md:ms-auto md:max-w-3/5 rounded-2xl px-5 py-3 bg-light-info">
   <Markdown message={message.content} kind="user" />
-
-  <!-- <div class="message-buttons-right">
-    <Copy value={message.content} />
-  </div> -->
 </div>
 
 <style>
