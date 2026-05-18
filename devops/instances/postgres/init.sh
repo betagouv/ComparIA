f6a0b309acfa5952c5b1a0ca84fd29ad090e0a5f
Elie Gavoty <eliegavoty@free.fr>
2026-05-18 16:49:34 +0200

    devops: add alembic migration tool + makefile entries

--- devops/instances/postgres/init.sh
+++ devops/instances/postgres/init.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 # Docker PostgreSQL entrypoint init script.
-# Creates comparia (via POSTGRES_DB) and comparia_da, applies schema to both.
+# Creates comparia (via POSTGRES_DB) and comparia_da.
+# Schema is applied via Alembic migrations (make db-migrate).
 
 set -e
 
-SCHEMA=$(sed \
-    -e "s/\"languia-dev\"/\"${POSTGRES_USER}\"/g" \
-    -e "s/\"languia-prd\"/\"${POSTGRES_USER}\"/g" \
-    -e "s/\"languia\"/\"${POSTGRES_USER}\"/g" \
-    /tmp/schema.sql)
-
-echo "$SCHEMA" | psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB"
-
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" \
     -c "CREATE DATABASE comparia_da OWNER \"${POSTGRES_USER}\";"
-
-echo "$SCHEMA" | psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "comparia_da"
