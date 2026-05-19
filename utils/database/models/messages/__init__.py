e7f6257574b89fb2c7b77d9ff7462d623e87280c
axolotle <nchesnais@mailoo.org>
2026-05-19 10:18:12 +0200

    fix: missing import

--- utils/database/models/messages/__init__.py
+++ utils/database/models/messages/__init__.py
@@ -1,4 +1,4 @@
-from .llm import LLMMessage, LLMMessageCreate, LLMMessageRead
+from .llm import LLMMessage, LLMMessageCreate, LLMMessageFinal, LLMMessageRead
 from .system import SystemMessage, SystemMessageCreate, SystemMessageRead
 from .user import UserMessage, UserMessageCreate, UserMessageRead
 
