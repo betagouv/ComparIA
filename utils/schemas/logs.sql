a5084d6c07b4b0bc525a357703d79fa671a78654
axolotle <nchesnais@mailoo.org>
2026-05-19 15:08:16 +0200

    chore: remove old utils/schemas sql folder

--- utils/schemas/logs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE logs (
-    time TIMESTAMP NOT NULL,
-    level VARCHAR(50) NOT NULL,
-    message TEXT NOT NULL,
-    query_params JSONB,
-    path_params JSONB,
-    session_hash VARCHAR(255),
-    extra JSONB
-);
