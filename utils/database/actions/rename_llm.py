036bfb954b0207629cf73fdebd20666249e31fdb
axolotle <nchesnais@mailoo.org>
2026-05-19 14:03:21 +0200

    refactor(lint): adapt rename_llm helper

--- utils/database/actions/rename_llm.py
+++ utils/database/actions/rename_llm.py
@@ -1,90 +1,39 @@
 import logging
 
-from sqlalchemy import text
+from sqlmodel import col, update
 
-from utils.utils import db_connection
-
-from ..utils import TABLE_NAMES, TableName
+from ..models import Comparison
+from ..session import get_session
 
 logger = logging.getLogger("comparia.db")
 
 
-RENAME_QUERY: dict[TableName, str] = {
-    "conversations": """
-        -- Allow migration of model name (rename model from old name to new name)
-        UPDATE 
-            conversations
-        SET
-            -- Replace either old name with the new name in model_a_name
-            model_a_name = CASE
-                WHEN model_a_name = '{old_name}' THEN '{new_name}'
-                ELSE model_a_name
-            END,
-
-            -- Replace either old name with the new name in model_b_name
-            model_b_name = CASE
-                WHEN model_b_name = '{old_name}' THEN '{new_name}'
-                ELSE model_b_name
-            END,
-
-            -- REPLACE old name in model_pair_name string
-            model_pair_name = REPLACE(model_pair_name, '{old_name}', '{new_name}')
-        WHERE
-            model_a_name = '{old_name}' 
-            OR model_b_name = '{old_name}' 
-            OR model_pair_name LIKE '%{old_name}%';
-""",
-    "votes": """
-        UPDATE votes
-        SET
-            -- Replace either old name with the new name in chosen_model_name
-            chosen_model_name = CASE
-                WHEN chosen_model_name = '{old_name}' THEN '{new_name}'
-                ELSE chosen_model_name
-            END
-        WHERE chosen_model_name = '{old_name}';
-""",
-    "reactions": """
-        UPDATE reactions
-        SET
-            -- Replace either old name with the new name in refers_to_model
-            refers_to_model = CASE
-                WHEN refers_to_model = '{old_name}' THEN '{new_name}'
-                ELSE refers_to_model
-            END
-        WHERE refers_to_model = '{old_name}';
-""",
-}
-
-
-def rename_llm(old_name: str, new_name: str, *, commit: bool = False):
+async def rename_llm(old_name: str, new_name: str, *, commit: bool = False):
     """
-    Rename an LLM id in all conversations, votes and reactions tables.
-    Will update columns:
-      - 'model_a_name', 'model_b_name' and 'model_pair_name' on conversations
-      - 'chosen_model_name' on votes
-      - 'refers_to_model' on reactions
+    Rename an LLM ids in all comparisons.
     """
-    logger.debug(f"Try to rename LLM {old_name} to {new_name} in database.")
+    logger.debug(f"Try to rename LLM '{old_name}' to '{new_name}' in comparisons.")
 
-    with db_connection() as conn:
-        for table_name in TABLE_NAMES:
-            results = conn.execute(
-                text(
-                    RENAME_QUERY[table_name].format(
-                        old_name=old_name, new_name=new_name
-                    )
-                )
+    row_count = 0
+    async with get_session() as session:
+        for k in ("llm_id_a", "llm_id_b"):
+            logger.debug(f"Searching for comparisons with '{old_name}' in '{k}'")
+            query = (
+                update(Comparison)
+                .where(col(getattr(Comparison, k)) == old_name)
+                .values({k: new_name})
             )
+            results = await session.exec(query)
+            row_count += results.rowcount
 
-            if not results.rowcount:
-                logger.info(f"No {table_name} with LLM '{old_name}' to rename found!")
-            elif commit:
-                conn.commit()
-                logger.info(
-                    f"Successfully renamed LLM '{old_name}' to '{new_name}' in {results.rowcount} {table_name}."
-                )
-            else:
-                logger.warning(
-                    f"Would have renamed LLM '{old_name}' to '{new_name}' in {results.rowcount} {table_name}."
-                )
+        if not row_count:
+            logger.info(f"No comparisons with LLM '{old_name}' to rename found!")
+        elif commit:
+            await session.commit()
+            logger.info(
+                f"Successfully renamed LLM '{old_name}' to '{new_name}' in {row_count} comparisons."
+            )
+        else:
+            logger.warning(
+                f"Would have renamed LLM '{old_name}' to '{new_name}' in {row_count} comparisons."
+            )
