7b27a140254d340bc2fe52792c28a57410f09c82
axolotle <nchesnais@mailoo.org>
2026-05-19 12:17:22 +0200

    chore(lint): drop archive_not_archived since only Comparison has the 'archived' field now

--- utils/database/actions/__init__.py
+++ utils/database/actions/__init__.py
@@ -1,6 +1,5 @@
 from .archive_corrupted import archive_corrupted
 from .archive_duplicate import archive_duplicate
-from .archive_not_archived import archive_not_archived
 from .archive_spam import archive_spam
 from .archive_unknown_llms import archive_unknown_llms
 from .llm_analyze import llm_analyze
