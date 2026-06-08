4e72b7c52e2e3111c2a2fd371794ad7cb7fe2ead
axolotle <nchesnais@mailoo.org>
2026-06-08 18:24:26 +0200

    feat(front): add arena layout

---
+++ frontend/src/routes/arene/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { NavBar } from '$components/header'
+
+  let { children } = $props()
+</script>
+
+<div class="lg:flex min-h-screen">
+  <NavBar />
+
+  <main class="lg:max-h-screen w-full overflow-y-auto">
+    {@render children()}
+  </main>
+</div>
