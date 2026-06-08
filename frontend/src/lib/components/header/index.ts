8a7fd0043be25aa063e0cff53e2abbd797a89a85
axolotle <nchesnais@mailoo.org>
2026-06-08 15:27:08 +0200

    feat(comp): add new component NavBar

--- frontend/src/lib/components/header/index.ts
+++ frontend/src/lib/components/header/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from './Header.svelte'
-export { default as Menubar } from './Menubar.svelte'
 export { default as LanguageSelector } from './LanguageSelector.svelte'
+export { default as Menubar } from './Menubar.svelte'
+export { default as NavBar } from './NavBar.svelte'
 export { default as VoteGauge } from './VoteGauge.svelte'
