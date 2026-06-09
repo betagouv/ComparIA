217812fdd39ee8ea0b2fb2b93a3c224ca86ec8d8
axolotle <nchesnais@mailoo.org>
2026-06-09 16:05:36 +0200

    refactor(front): use PageLayout in arena page

--- frontend/src/routes/arene/+page.svelte
+++ frontend/src/routes/arene/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { Link } from '$components/dsfr'
-  import SeoHead from '$components/SEOHead.svelte'
+  import PageLayout from '$components/PageLayout.svelte'
   import { fetchAndSolveSilently } from '$lib/captcha.svelte'
   import { getComparison, initComparisonsContext } from '$lib/chatService.svelte'
   import { m } from '$lib/i18n/messages'
@@ -14,57 +14,32 @@
   const comparator = getComparison(undefined)
   const showInitialPrompt = $derived(!comparator.comparisonId)
 
-  // Compute second header height for autoscrolling
-  let secondHeader = $state<HTMLElement>()
-  let secondHeaderSize = $derived<number>(secondHeader?.offsetHeight ?? 0)
-
-  function onResize() {
-    secondHeaderSize = secondHeader ? secondHeader.offsetHeight : 0
-  }
+  const revealed = $derived(comparator.status === 'revealed')
 </script>
-
-<svelte:window onresize={onResize} />
-
-<SeoHead title={m['seo.titles.arene']()} />
 
 <TOSModal />
 
-{#if comparator.status === 'revealed'}
-  <div
-    bind:this={secondHeader}
-    id="second-header"
-    class="fr-container--fluid bg-light-grey top-0 py-3 md:py-4 sticky z-3 drop-shadow-[--raised-shadow]"
-  >
-    <div class="fr-container gap-3 md:flex-row flex flex-col items-center">
-      <div class="gap-3 md:flex-row flex basis-2/3 flex-col items-center">
-        <div
-          class="bg-primary px-4 py-2 font-bold text-white rounded-[3.75rem] text-[18px] text-nowrap"
-        >
-          {m['header.chatbot.reveal.title']()}
-        </div>
-        <div class="md:text-left flex flex-col text-center">
-          <strong class="text-dark-grey">
-            {m['header.chatbot.reveal.subtitle']()}
-          </strong>
-          <p class="mt-2! mb-0! text-sm! leading-normal! text-grey md:mt-0!">
-            {m['header.chatbot.reveal.description']()}
-          </p>
-        </div>
-      </div>
-      <div class="md:block hidden w-full basis-1/3 items-center text-right">
-        <Link
-          button
-          icon="edit-line"
-          href="../arene/?cgu_acceptees"
-          text={m['header.chatbot.newDiscussion']()}
-          class="md:w-auto! w-full!"
-        />
-      </div>
+<PageLayout
+  seoTitle={m['seo.titles.arene']()}
+  title={m[revealed ? 'header.chatbot.reveal.description' : 'seo.titles.arene']()}
+  subtitle={revealed ? m['header.chatbot.reveal.subtitle']() : undefined}
+  bubble={revealed ? m['header.chatbot.reveal.title']() : undefined}
+  class="bg-very-light-grey p-0! relative flex min-h-full flex-col"
+  stickyHeader
+  headerClass={{ 'sr-only!': !revealed }}
+>
+  {#snippet header()}
+    <div class={['ms-auto hidden items-center', { 'md:block': revealed }]}>
+      <Link
+        button
+        icon="edit-line"
+        href="../arene/?cgu_acceptees"
+        text={m['header.chatbot.newDiscussion']()}
+        class="text-nowrap"
+      />
     </div>
-  </div>
-{/if}
+  {/snippet}
 
-<div class="bg-very-light-grey relative" style="--second-header-size: {secondHeaderSize}px;">
   {#if showInitialPrompt}
     <ViewPrompt
       loading={comparator.loading}
@@ -74,4 +49,4 @@
   {:else}
     <ViewChat comparisonId={comparator.comparisonId!} />
   {/if}
-</div>
+</PageLayout>
