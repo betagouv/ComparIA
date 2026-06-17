5f9893913735571a9c58b700b039d3427e5ccc17
axolotle <nchesnais@mailoo.org>
2026-06-17 15:55:14 +0200

    feat(cli): add internal commands

---
+++ internal/cli.py
@@ -0,0 +1,14 @@
+import logging
+
+from cyclopts import App
+
+from utils.logger import configure_logger
+
+from .typescript.generate_frontend_types import generate_frontend_types
+
+cli_internal = App(name="internal", help="Project utilities.")
+cli_internal.command(generate_frontend_types, name="generate_types")
+
+if __name__ == "__main__":
+    configure_logger(logging.getLogger("comparia"))
+    cli_internal()
