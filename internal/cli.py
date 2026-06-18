ca914e974d07cb77b5a5ef306f46e9baabea6ea6
axolotle <nchesnais@mailoo.org>
2026-06-18 15:04:52 +0200

    feat(cli): add front constants and archs generation command and functions

--- internal/cli.py
+++ internal/cli.py
@@ -4,10 +4,14 @@ from cyclopts import App
 
 from utils.logger import configure_logger
 
-from .typescript.generate_frontend_types import generate_frontend_types
+from .typescript.generate_frontend_types import (
+    generate_frontend_constants,
+    generate_frontend_types,
+)
 
 cli_internal = App(name="internal", help="Project utilities.")
 cli_internal.command(generate_frontend_types, name="generate_types")
+cli_internal.command(generate_frontend_constants, name="generate_constants")
 
 if __name__ == "__main__":
     configure_logger(logging.getLogger("comparia"))
--- internal/typescript/generate_frontend_types.py
+++ internal/typescript/generate_frontend_types.py
@@ -1,13 +1,29 @@
 from pydantic2ts import generate_typescript_defs
 
+from internal.archs import get_archs
 from utils.utils import FRONTEND_DIR, FRONTEND_GENERATED_DIR
 
 JSON2TS_PATH = FRONTEND_DIR / "node_modules/.bin/json2ts"
+FRONTEND_TYPES = FRONTEND_GENERATED_DIR / "backend.ts"
+FRONTEND_CONSTANTS = FRONTEND_GENERATED_DIR / "constants.ts"
 
 
 def generate_frontend_types():
     generate_typescript_defs(
         "internal.typescript.backend_types",
-        str(FRONTEND_GENERATED_DIR / "backend.ts"),
+        str(FRONTEND_TYPES),
         json2ts_cmd=str(JSON2TS_PATH.absolute()),
     )
+
+
+def generate_frontend_constants() -> None:
+    archs = [arch.id for arch in get_archs().root]
+
+    constants = {
+        "ARCHS": archs,
+        "MAYBE_ARCHS": [f"maybe-{a}" for a in archs if a != "na"],
+    }
+
+    file_lines = [f"export const {k} = {v} as const" for k, v in constants.items()]
+
+    FRONTEND_CONSTANTS.write_text("\n".join(file_lines) + "\n")
