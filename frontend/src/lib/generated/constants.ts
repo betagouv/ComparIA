b3ff411f78377159850a905d34146e131ed16385
axolotle <nchesnais@mailoo.org>
2026-06-19 19:13:47 +0200

    chore(types): update generated

--- frontend/src/lib/generated/constants.ts
+++ frontend/src/lib/generated/constants.ts
@@ -1,2 +1,8 @@
+export const SIZE_CLASSES = ['XS', 'S', 'M', 'L', 'XL'] as const
+export const ENERGY_CLASSES = ['A', 'B', 'C', 'D', 'E', 'F'] as const
 export const ARCHS = ['moe', 'dense', 'matformer', 'na'] as const
 export const MAYBE_ARCHS = ['maybe-moe', 'maybe-dense', 'maybe-matformer'] as const
+export type SizeClasses = (typeof SIZE_CLASSES)[number]
+export type EnergyClasses = (typeof ENERGY_CLASSES)[number]
+export type Archs = (typeof ARCHS)[number]
+export type MaybeArchs = (typeof MAYBE_ARCHS)[number]
