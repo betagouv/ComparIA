747a27bbffb0739737413e167c3a91e03f11392d
axolotle <nchesnais@mailoo.org>
2026-06-25 13:32:56 +0200

    feat(cli): add generate-i18n

--- internal/cli.py
+++ internal/cli.py
@@ -4,6 +4,7 @@ from cyclopts import App
 
 from utils.logger import configure_logger
 
+from .archs import generate_archs_i18n
 from .typescript.generate_frontend_types import (
     generate_frontend_constants,
     generate_frontend_types,
@@ -12,6 +13,7 @@ from .typescript.generate_frontend_types import (
 cli_internal = App(name="internal", help="Project utilities.")
 cli_internal.command(generate_frontend_types, name="generate_types")
 cli_internal.command(generate_frontend_constants, name="generate_constants")
+cli_internal.command(generate_archs_i18n, name="generate_i18n")
 
 if __name__ == "__main__":
     configure_logger(logging.getLogger("comparia"))
