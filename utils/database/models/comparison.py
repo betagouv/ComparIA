d6126ceb64c5d857f14b8bce8f02e1e1b90cf056
Elie Gavoty <eliegavoty@free.fr>
2026-06-30 15:03:14 +0200

    feat(db): db models creation for email auth

--- utils/database/models/comparison.py
+++ utils/database/models/comparison.py
@@ -54,6 +54,7 @@ class ErrorDetails(BaseModel):
 class ComparisonBase(BaseDBModel):
     ip: str  # WARNING: PII
     visitor_id: str | None = None
+    user_id: Annotated[uuid.UUID | None, Field(foreign_key="auth_user.id")] = None
     cohorts: Annotated[str | None, StripAndEmptyAsNone] = None
     mode: Annotated[SelectionMode, Field(sa_type=String)]
     custom_models_selection: Annotated[CustomModelsSelection, Field(sa_type=JSONB)] = (
