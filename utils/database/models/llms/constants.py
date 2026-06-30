9fa9f65dcd405461ad3b9554679fe10ac6664d51
axolotle <nchesnais@mailoo.org>
2026-06-30 13:27:16 +0200

    feat(db): add arch 'maybe-matformer' for consistency

--- utils/database/models/llms/constants.py
+++ utils/database/models/llms/constants.py
@@ -1,5 +1,7 @@
 from typing import Literal
 
-LLMArchKind = Literal["moe", "matformer", "dense", "maybe-moe", "maybe-dense", "na"]
+LLMArchKind = Literal[
+    "moe", "matformer", "dense", "maybe-moe", "maybe-matformer", "maybe-dense", "na"
+]
 LLMLicenseKind = Literal["proprietary", "open-weights", "open-source"]
 LLMStatus = Literal["archived", "disabled", "enabled"]
