5c97684f732aa6c8d1149d2175073b2d7f72901d
axolotle <nchesnais@mailoo.org>
2026-07-03 17:05:13 +0200

    feat(front): add quick comparison list in NavBar

---
+++ frontend/src/routes/arene/+layout.ts
@@ -0,0 +1,16 @@
+import { api } from '$lib/fastapi-client'
+import type { LayoutLoad } from './$types'
+
+export const ssr = false // auth error on server side
+
+export const load: LayoutLoad = async () => {
+  // FIXME types
+  const comparisons = await api.request<unknown>('/arena/comparison/list', {
+    credentials: 'include',
+    method: 'GET'
+  })
+
+  return {
+    comparisons
+  }
+}
