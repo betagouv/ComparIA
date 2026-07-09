6befd2eea4f921c24d966b8f0da4a02b557b0b61
axolotle <nchesnais@mailoo.org>
2026-07-09 13:49:26 +0200

    feat(admin): add llms layout

---
+++ frontend/src/routes/(admin)/admin/llms/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { resolve } from '$app/paths'
+  import TabNav from '$components/dsfr/TabNav.svelte'
+  import PageLayout from '$components/PageLayout.svelte'
+  import { m } from '$lib/i18n/messages'
+
+  import type { LayoutProps } from './$types'
+
+  let { children }: LayoutProps = $props()
+
+  const links = [
+    { id: 'list', icon: 'trophy-line', label: 'LLMs', href: resolve('/admin/llms/list') }
+  ]
+</script>
+
+<PageLayout
+  seoTitle={m['admin.llms.title']()}
+  title={m['admin.llms.title']()}
+  subtitle={m['admin.llms.subTitle']()}
+>
+  <TabNav {links} class="mb-5" />
+
+  {@render children()}
+</PageLayout>
