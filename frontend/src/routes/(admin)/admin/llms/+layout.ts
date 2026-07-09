1cd56bca497da071126367ad9e2bd909169bcef9
axolotle <nchesnais@mailoo.org>
2026-07-09 16:59:34 +0200

    refactor(admin): update llm data front query

--- frontend/src/routes/(admin)/admin/llms/+layout.ts
+++ frontend/src/routes/(admin)/admin/llms/+layout.ts
@@ -1,14 +1,16 @@
 import { api } from '$lib/fastapi-client'
-import type { APILLMData } from '$lib/generated/backend'
+import type { LLMData, LLMEndpoint, LLMLab, LLMLicense } from '$lib/generated/admin'
 import type { LayoutLoad } from './$types'
 
 export const ssr = false // auth error on server side
 
 export const load: LayoutLoad = async () => {
-  // FIXME types
-  const llms = await api.request<APILLMData[]>('/admin/llms/list')
+  const data = await api.request<{
+    endpoints: LLMEndpoint[]
+    licenses: LLMLicense[]
+    labs: LLMLab[]
+    llms: LLMData[]
+  }>('/admin/llms/data')
 
-  return {
-    llms
-  }
+  return data
 }
