a080877b5ebdd5a2072d53c630df0f93ef3477f6
axolotle <nchesnais@mailoo.org>
2026-07-09 16:48:28 +0200

    feat(front): add toSearchString helper

--- frontend/src/lib/utils/data.ts
+++ frontend/src/lib/utils/data.ts
@@ -1,3 +1,5 @@
+import type { ClassValue } from 'svelte/elements'
+
 export function sortIfDefined(a: Record<string, any>, b: Record<string, any>, key: string) {
   if (a[key] !== undefined && b[key] !== undefined) return b[key] - a[key]
   if (a[key] !== undefined) return -1
@@ -70,4 +72,8 @@ export function sortRows<
           return a[options.col].localeCompare(b[options.col])
       }
     })
+}
+
+export function toSearchString(arr: string[]) {
+  return arr.map((str) => str.toLowerCase()).join(' ')
 }
