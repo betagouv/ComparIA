c7def60e2a0beb143948c2f3c8d8e48d6d05b087
axolotle <nchesnais@mailoo.org>
2026-07-13 14:20:36 +0200

    feat(utils): add custom json schema generator to help with auto form generation

--- utils/utils.py
+++ utils/utils.py
@@ -6,6 +6,7 @@ from typing import Any, TypeVar, overload
 import markdown
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, FieldSerializationInfo, PlainSerializer
+from pydantic.json_schema import GenerateJsonSchema, JsonSchemaValue
 
 from utils.logger import configure_logger
 
@@ -103,3 +104,16 @@ def sort_dict(data: Obj, deep: bool = True) -> Obj:
     ]
 
     return dict(sorted(items, key=lambda i: i[0].lower()))
+
+
+class FormJsonSchema(GenerateJsonSchema):
+    def sort(
+        self, value: JsonSchemaValue, parent_key: str | None = None
+    ) -> JsonSchemaValue:
+        # Do not sort at all, form will be displayed based on attribute order
+        return value
+
+    def get_flattened_anyof(self, schemas: list[JsonSchemaValue]) -> JsonSchemaValue:
+        # remove null values for simplicity, required fields will be handle by the back
+        non_null = [schema for schema in schemas if schema["type"] != "null"]
+        return super().get_flattened_anyof(schemas=non_null)
