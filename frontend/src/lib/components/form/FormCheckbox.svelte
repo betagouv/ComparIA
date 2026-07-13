8f6768a1a7e72a04d24e1b7b0e313f9c86c2f06d
axolotle <nchesnais@mailoo.org>
2026-07-13 14:05:26 +0200

    feat(front): add FormCheckbox component

---
+++ frontend/src/lib/components/form/FormCheckbox.svelte
@@ -0,0 +1,26 @@
+<script module lang="ts">
+  export type FormCheckboxProps = BaseFormFieldProps<'checkbox', boolean>
+</script>
+
+<script lang="ts">
+  import type { BaseFormFieldProps } from '$lib/utils/form'
+  import { FormField } from '.'
+
+  let { value = $bindable(), ...props }: FormCheckboxProps = $props()
+</script>
+
+<FormField {...props} component="checkbox">
+  {#snippet formItem({ ...fieldProps })}
+    <input {...fieldProps} type="checkbox" bind:checked={value} />
+  {/snippet}
+</FormField>
+
+<style lang="postcss">
+  input[type='checkbox'] {
+    :global(& + label) {
+      --border-action-high-blue-france: var(--blue-france-main-525);
+      --border-active-blue-france: var(--blue-france-main-525);
+      --background-active-blue-france: var(--blue-france-main-525);
+    }
+  }
+</style>
