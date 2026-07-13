dcae49a325869476f69ce31d26951011974c5697
axolotle <nchesnais@mailoo.org>
2026-07-13 14:08:03 +0200

    feat(front): add FormToggle component

---
+++ frontend/src/lib/components/form/FormToggle.svelte
@@ -0,0 +1,61 @@
+<script module lang="ts">
+  export type FormToggleProps = {
+    checkKind?: 'truth' | 'active'
+  } & BaseFormFieldProps<'toggle', boolean>
+</script>
+
+<script lang="ts">
+  import { m } from '$lib/i18n/messages'
+  import type { BaseFormFieldProps } from '$lib/utils/form'
+
+  let {
+    value = $bindable(),
+    id,
+    label,
+    help,
+    errors,
+    checkKind = 'truth'
+  }: FormToggleProps = $props()
+
+  const messagesId = $derived(`${id}-messages`)
+  const hintId = $derived(`${id}-hint`)
+  const checkedLabel = $derived(m[`words.${checkKind === 'truth' ? 'yes' : 'activated'}`]())
+  const uncheckedLabel = $derived(m[`words.${checkKind === 'truth' ? 'no' : 'deactivated'}`]())
+  const error = $derived(errors?.[id])
+</script>
+
+<div class={['fr-toggle mb-6!', { 'fr-toggle--error': !!error }]}>
+  <input
+    {id}
+    type="checkbox"
+    bind:checked={value}
+    class="fr-toggle__input"
+    aria-describedby="{hintId} {messagesId}"
+  />
+
+  <label
+    class="fr-toggle__label"
+    for={id}
+    data-fr-checked-label={checkedLabel}
+    data-fr-unchecked-label={uncheckedLabel}
+  >
+    {label}
+  </label>
+
+  {#if help}
+    <span id={hintId} class="fr-hint-text">{help}</span>
+  {/if}
+
+  {#if error}
+    <div class="fr-messages-group" id={messagesId} aria-live="polite">
+      <p class="fr-message fr-message--error">{error}</p>
+    </div>
+  {/if}
+</div>
+
+<style lang="postcss">
+  input[type='checkbox'] {
+    --border-action-high-blue-france: var(--blue-france-main-525);
+    --text-active-blue-france: var(--blue-france-main-525);
+  }
+</style>
