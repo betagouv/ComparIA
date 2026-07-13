1be065d5f77209194a9990d219591ac9197e4a03
axolotle <nchesnais@mailoo.org>
2026-07-13 14:10:22 +0200

    feat(front): add meta AnyFormItem, FormFieldsetItem & FormFieldsetList components

---
+++ frontend/src/lib/components/form/FormFieldsetItem.svelte
@@ -0,0 +1,31 @@
+<script module lang="ts">
+  export type FormFieldsetItemProps = {
+    subProps: AnyFormItemProps[]
+  } & BaseFormFieldProps<'fieldset-item'>
+</script>
+
+<script lang="ts">
+  import AnyFormItem from '$components/form/AnyFormItem.svelte'
+  import type { AnyFormItemProps, BaseFormFieldProps } from '$lib/utils/form'
+  import { FormFieldset } from '.'
+
+  let { value = $bindable(), subProps, ...props }: FormFieldsetItemProps = $props()
+</script>
+
+<FormFieldset {...props} component="fieldset">
+  {#snippet formItem()}
+    {#each subProps as sub, i (i)}
+      <div class="fr-fieldset__element">
+        <AnyFormItem
+          {...sub}
+          id="{props.id}-{sub.id}"
+          bind:value={value[sub.id]}
+          errors={props.errors}
+        />
+      </div>
+    {/each}
+  {/snippet}
+</FormFieldset>
+
+<style lang="postcss">
+</style>
