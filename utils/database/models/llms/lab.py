e223bcd8515dbb83def0a8b44f352546bb421cfb
axolotle <nchesnais@mailoo.org>
2026-07-13 15:16:50 +0200

    refactor(db): add title+description on LLMEndpoint, LLMLab & LLMLicense

--- utils/database/models/llms/lab.py
+++ utils/database/models/llms/lab.py
@@ -1,25 +1,29 @@
+from typing import Annotated
+
+from sqlmodel import Field
+
 from ..utils import BaseDBModel
+
+FIELDS = {
+    "name": {},
+    "logo": {
+        "description": "An icon name from https://lobehub.com/fr/icons or a filename (e.g. 'ai2.svg') from `frontend/static/orgs/ai/`."
+    },
+    "origin_country": {
+        "description": "A 2 letter code from https://en.wikipedia.org/wiki/ISO_3166-1."
+    },
+}
 
 
 class LLMLabBase(BaseDBModel):
-    name: str
-    logo: str  # icon name, or file?
-    origin_country: str  # FIXME use lib?
+    name: Annotated[str, Field(**FIELDS["name"])]
+    logo: Annotated[str, Field(**FIELDS["logo"])]  # icon name, or file?
+    origin_country: Annotated[str, Field(**FIELDS["origin_country"])]  # FIXME use lib?
 
 
 class LLMLab(LLMLabBase, table=True):
     """
     LLM lab/organization metadata.
-
-    Attributes
-    ----------
-    name
-        Lab name.
-    logo
-        An icon name from https://lobehub.com/fr/icons or a filename
-        (e.g. 'ai2.svg') from `frontend/static/orgs/ai/`.
-    origin_country
-        Lab's origin country as a 2 letter code from https://en.wikipedia.org/wiki/ISO_3166-1.
     """
 
     __tablename__ = "llm_lab"
