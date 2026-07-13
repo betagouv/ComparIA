c1cb7f409d98c9de271ffcaa69dd8589e3e3b1d2
axolotle <nchesnais@mailoo.org>
2026-07-13 14:43:10 +0200

    feat(admin): add llm item upsert routes

--- backend/admin/llms/router.py
+++ backend/admin/llms/router.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Literal
+from uuid import UUID
 
 from fastapi import APIRouter
 from sqlmodel import SQLModel, select
@@ -15,11 +16,20 @@ from utils.database.models.llms import (
     LLMLicenseUpsert,
 )
 from utils.database.session import get_session
+from utils.llms.services import (
+    upsert_llm,
+    upsert_llm_endpoint,
+    upsert_llm_lab,
+    upsert_llm_license,
+)
 from utils.utils import FormJsonSchema
 
 logger = logging.getLogger("languia")
 
 router = APIRouter(prefix="/llms", tags=["llms"])
+
+# FIXME temp
+COMMIT = False
 
 ModelKind = Literal["endpoints", "licenses", "labs", "llms"]
 MODELS_UPSERT: dict[ModelKind, SQLModel] = {
@@ -57,3 +67,31 @@ async def get_schema():
         k: model.model_json_schema(schema_generator=FormJsonSchema)
         for k, model in MODELS_UPSERT.items()
     }
+
+
+@router.put("/llm/{item_id}")
+async def update_llm(item_id: UUID, body: LLMDataUpsert) -> LLMData:
+    body.id = item_id
+    async with get_session() as session:
+        return await upsert_llm(body, session, commit=COMMIT)
+
+
+@router.put("/endpoint/{item_id}")
+async def update_endpoint(item_id: UUID, body: LLMEndpointUpsert) -> LLMEndpoint:
+    body.id = item_id
+    async with get_session() as session:
+        return await upsert_llm_endpoint(body, session, commit=COMMIT)
+
+
+@router.put("/lab/{item_id}")
+async def update_lab(item_id: UUID, body: LLMLabUpsert) -> LLMLab:
+    body.id = item_id
+    async with get_session() as session:
+        return await upsert_llm_lab(body, session, commit=COMMIT)
+
+
+@router.put("/license/{item_id}")
+async def update_license(item_id: UUID, body: LLMLicenseUpsert) -> LLMLicense:
+    body.id = item_id
+    async with get_session() as session:
+        return await upsert_llm_license(body, session, commit=COMMIT)
