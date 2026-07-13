69b2ce8be190e7c69f57420b72a6e69df1739281
axolotle <nchesnais@mailoo.org>
2026-07-13 14:05:26 +0200

    feat(front): add FormCheckbox component

--- frontend/src/lib/components/form/index.ts
+++ frontend/src/lib/components/form/index.ts
@@ -1,8 +1,10 @@
+export { default as FormCheckbox } from './FormCheckbox.svelte'
 export { default as FormField } from './FormField.svelte'
 export { default as FormFieldset } from './FormFieldset.svelte'
 export { default as FormInput } from './FormInput.svelte'
 export { default as FormSelect } from './FormSelect.svelte'
 
+export type { FormCheckboxProps } from './FormCheckbox.svelte'
 export type { FormFieldProps } from './FormField.svelte'
 export type { FormFieldsetProps } from './FormFieldset.svelte'
 export type { FormInputProps } from './FormInput.svelte'
