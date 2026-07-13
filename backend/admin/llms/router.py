21bbdfd81a9b5bf21d8066353423ac60fdeec7be
axolotle <nchesnais@mailoo.org>
2026-07-13 14:25:46 +0200

    feat(admin): add /schemas route to query llm data items schemas

--- backend/admin/llms/router.py
+++ backend/admin/llms/router.py
@@ -1,14 +1,33 @@
 import logging
+from typing import Literal
 
 from fastapi import APIRouter
-from sqlmodel import select
+from sqlmodel import SQLModel, select
 
-from utils.database.models.llms import LLMData, LLMEndpoint, LLMLab, LLMLicense
+from utils.database.models.llms import (
+    LLMData,
+    LLMDataUpsert,
+    LLMEndpoint,
+    LLMEndpointUpsert,
+    LLMLab,
+    LLMLabUpsert,
+    LLMLicense,
+    LLMLicenseUpsert,
+)
 from utils.database.session import get_session
+from utils.utils import FormJsonSchema
 
 logger = logging.getLogger("languia")
 
 router = APIRouter(prefix="/llms", tags=["llms"])
+
+ModelKind = Literal["endpoints", "licenses", "labs", "llms"]
+MODELS_UPSERT: dict[ModelKind, SQLModel] = {
+    "endpoints": LLMEndpointUpsert,
+    "licenses": LLMLicenseUpsert,
+    "labs": LLMLabUpsert,
+    "llms": LLMDataUpsert,
+}
 
 
 @router.get("/data")
@@ -30,3 +49,11 @@ async def get_data():
     except Exception as e:
         logger.error(f"[DB] Error loading LLMsData: {e}")
         raise
+
+
+@router.get("/schemas")
+async def get_schema():
+    return {
+        k: model.model_json_schema(schema_generator=FormJsonSchema)
+        for k, model in MODELS_UPSERT.items()
+    }
