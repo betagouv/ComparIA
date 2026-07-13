d28351495d2cdb54807cca87c8215a6370672311
axolotle <nchesnais@mailoo.org>
2026-07-13 14:02:30 +0200

    feat(front): add FormInput component

--- frontend/src/lib/components/form/index.ts
+++ frontend/src/lib/components/form/index.ts
@@ -1,5 +1,7 @@
 export { default as FormField } from './FormField.svelte'
 export { default as FormFieldset } from './FormFieldset.svelte'
+export { default as FormInput } from './FormInput.svelte'
 
 export type { FormFieldProps } from './FormField.svelte'
 export type { FormFieldsetProps } from './FormFieldset.svelte'
+export type { FormInputProps } from './FormInput.svelte'
