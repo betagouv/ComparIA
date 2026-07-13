15b0d6d04fb06c5e12f2d86a79228bedff930319
axolotle <nchesnais@mailoo.org>
2026-07-13 14:03:34 +0200

    feat(front): add FormSelect component

---
+++ frontend/src/lib/components/form/FormSelect.svelte
@@ -0,0 +1,29 @@
+<script module lang="ts">
+  export type FormSelectProps = {
+    options: Option<string>[]
+  } & BaseFormFieldProps<'select', string>
+</script>
+
+<script lang="ts">
+  import type { BaseFormFieldProps, Option } from '$lib/utils/form'
+  import { FormField } from '.'
+
+  let { value = $bindable(), options, ...props }: FormSelectProps = $props()
+</script>
+
+<FormField {...props} component="select">
+  {#snippet formItem(fieldProps)}
+    <select {...props} {...fieldProps} bind:value class="fr-select">
+      {#each options as option (option.value)}
+        <option value={option.value}>{option.label}</option>
+      {/each}
+    </select>
+  {/snippet}
+</FormField>
+
+<style lang="postcss">
+  .fr-select {
+    --border-plain-grey: var(--blue-france-main-525);
+    --background-contrast-grey: var(--blue-ecume-975-75);
+  }
+</style>
--- frontend/src/lib/components/form/index.ts
+++ frontend/src/lib/components/form/index.ts
@@ -1,6 +1,7 @@
 export { default as FormField } from './FormField.svelte'
 export { default as FormFieldset } from './FormFieldset.svelte'
 export { default as FormInput } from './FormInput.svelte'
+export { default as FormSelect } from './FormSelect.svelte'
 
 export type { FormFieldProps } from './FormField.svelte'
 export type { FormFieldsetProps } from './FormFieldset.svelte'
