40c6c67f19452da4b596744d60c76e641ff5e82c
axolotle <nchesnais@mailoo.org>
2026-07-14 14:39:31 +0200

    refactor(bo): update llm items pages to handle both create and update

--- frontend/src/routes/(admin)/admin/llms/llms/[id]/+page.svelte
+++ frontend/src/routes/(admin)/admin/llms/llms/[id]/+page.svelte
@@ -1,52 +1,34 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
+  import { resolve } from '$app/paths'
   import { page } from '$app/state'
   import Form from '$components/form/Form.svelte'
-  import { omit } from '$lib/utils/commons'
-  import { getFormFields } from '$lib/utils/form'
+  import { useForm } from '$lib/stores/form.svelte'
   import type { PageProps } from './$types'
 
-  const { data: _data }: PageProps = $props()
+  const { data }: PageProps = $props()
 
-  const omittedKeys = ['id', 'updated_at', 'created_at'] as const
-  const data = $derived(_data.llms.find((item) => item.id === page.params.id)!)
-  const schema = $derived.by(() => {
-    const schema = _data.schemas.llms
-    schema.properties!.lab_id.type = 'string'
-    schema.properties!.lab_id.enum = _data.labs.map((l) => ({ label: l.name, value: l.id! }))
-    schema.properties!.license_id.type = 'string'
-    schema.properties!.license_id.enum = _data.licenses.map((l) => ({
-      label: l.name,
-      value: l.id!
-    }))
-    schema.properties!.endpoint_id.type = 'string'
-    schema.properties!.endpoint_id.enum = _data.endpoints.map((e) => ({
-      label: e.name,
-      value: e.id!
-    }))
-    return schema
-  })
-  const form = $state({
-    ...omit(data, omittedKeys),
-    knowledge_cutoff: data.knowledge_cutoff
-      ? new Date(data.knowledge_cutoff).toISOString().split('T')[0]
-      : undefined,
-    release_date: new Date(data.release_date).toISOString().split('T')[0]
-  })
-  const fields = $derived(
-    getFormFields(schema, 'llm_upsert').filter((f) => !omittedKeys.includes(f.id))
+  const id = $derived(page.params.id)
+  const method = $derived(id === 'create' ? 'post' : 'put')
+  const form = $derived(
+    useForm({
+      url: '/admin/llms/llm',
+      ...data.formProps,
+      omitKeys: ['updated_at', 'created_at'],
+      i18nKey: 'llm_upsert',
+      method,
+      onSuccess: (updated) => {
+        Object.assign(data.formProps.data, updated)
+        if (method === 'post') {
+          // Update app data
+          data.llms.push(updated)
+          goto(resolve(`/admin/llms/llms/${updated.id}`))
+        }
+      }
+    })
   )
-  let errors = $state({})
 </script>
 
 <div>
-  <Form
-    id={data.id}
-    label="LLM"
-    subLabel={data.id}
-    items={fields}
-    {form}
-    {errors}
-    url={`/admin/llms/llm/${data.id}`}
-    method="put"
-  />
+  <Form {id} label="LLM" subLabel={id} {...form} />
 </div>
