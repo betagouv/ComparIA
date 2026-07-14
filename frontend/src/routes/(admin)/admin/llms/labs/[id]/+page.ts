b88263221c18554b30cf10adf6b2abd719e3ce47
axolotle <nchesnais@mailoo.org>
2026-07-14 14:36:40 +0200

    feat(bo): add llm items +page to handle 404 errors and simplify data gathering

---
+++ frontend/src/routes/(admin)/admin/llms/labs/[id]/+page.ts
@@ -0,0 +1,13 @@
+import type { LLMLab } from '$lib/generated/admin'
+import { error } from '@sveltejs/kit'
+import type { PageLoad } from './$types'
+
+export const load: PageLoad = async ({ parent, params }) => {
+  const { labs, schemas } = await parent()
+  const data = labs.find((item) => item.id === params.id)
+  if (!data && params.id !== 'create') error(404)
+
+  return {
+    formProps: { schema: schemas.labs, data: data ?? ({} as LLMLab) }
+  }
+}
