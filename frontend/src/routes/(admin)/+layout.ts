5db06ad0b9ff1f00ded6fd28380f7a478667da65
axolotle <nchesnais@mailoo.org>
2026-07-16 15:17:22 +0200

    fix(front): wrong fetch

--- frontend/src/routes/(admin)/+layout.ts
+++ frontend/src/routes/(admin)/+layout.ts
@@ -3,7 +3,7 @@ import { api } from '$lib/fastapi-client'
 import { redirect } from '@sveltejs/kit'
 import type { LayoutLoad } from './$types'
 
-export const load: LayoutLoad = async ({ url }) => {
+export const load: LayoutLoad = async ({ url, fetch }) => {
   // Requery auth in case of recent login, parent raw data may not be updated
   const auth = await api.request<{ user: AuthUser | null }>('/auth/me', { fetch })
 
