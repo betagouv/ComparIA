50caf01cf3508e02788c9d02d3f4a13ce0b28d26
Elie Gavoty <eliegavoty@free.fr>
2026-08-12 16:58:58 +0200

    fix(db): merge all three alembic heads (oidc, vote_tags, publish_frequency)
    The merge revision only pulled in two of three heads branching from
    f8c2b6a41d70: a1b2c3d4e5f6 (oidc) and f4a8c2d9e1b7 (publish_frequency),
    leaving b3d1c7a4e9f2 (vote_tags) as a separate head. Add it to
    down_revision so 'alembic upgrade head' resolves to a single head.

--- utils/database/alembic/versions/8fc0116d17b7_merge_oidc_and_publish_frequency_heads.py
+++ utils/database/alembic/versions/8fc0116d17b7_merge_oidc_and_publish_frequency_heads.py
@@ -1,7 +1,7 @@
-"""merge_oidc_and_publish_frequency_heads
+"""merge_oidc_publish_frequency_and_vote_tags_heads
 
 Revision ID: 8fc0116d17b7
-Revises: a1b2c3d4e5f6, f4a8c2d9e1b7
+Revises: a1b2c3d4e5f6, b3d1c7a4e9f2, f4a8c2d9e1b7
 Create Date: 2026-08-12 12:55:00.159381
 
 """
@@ -14,7 +14,7 @@ import sqlmodel
 
 # revision identifiers, used by Alembic.
 revision: str = '8fc0116d17b7'
-down_revision: Union[str, Sequence[str], None] = ('a1b2c3d4e5f6', 'f4a8c2d9e1b7')
+down_revision: Union[str, Sequence[str], None] = ('a1b2c3d4e5f6', 'b3d1c7a4e9f2', 'f4a8c2d9e1b7')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
