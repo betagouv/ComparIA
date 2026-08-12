18901634691b9f557354909f2b5b1ec2f88c5a33
Elie Gavoty <eliegavoty@free.fr>
2026-08-12 12:55:23 +0200

    fix(db): merge OIDC and publish-frequency alembic heads

---
+++ utils/database/alembic/versions/8fc0116d17b7_merge_oidc_and_publish_frequency_heads.py
@@ -0,0 +1,29 @@
+"""merge_oidc_and_publish_frequency_heads
+
+Revision ID: 8fc0116d17b7
+Revises: a1b2c3d4e5f6, f4a8c2d9e1b7
+Create Date: 2026-08-12 12:55:00.159381
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel
+
+
+# revision identifiers, used by Alembic.
+revision: str = '8fc0116d17b7'
+down_revision: Union[str, Sequence[str], None] = ('a1b2c3d4e5f6', 'f4a8c2d9e1b7')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
